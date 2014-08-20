6a5e7092b81b204a7f2c6c36b2ef58caee5937a8
mgleason <michael.gleason@nrel.gov>
2014-08-20 17:55:17 -0600

    Last step in enabling user-defined rate escalations -- interpolating odd years and extending to 2080. Closes #192

--- sql/data_prep/6_create_views.sql
+++ sql/data_prep/6_create_views.sql
@@ -223,68 +223,64 @@ order by year, sector;
 
 -- create view for rate escalations
 CREATE OR REPLACE VIEW diffusion_wind.rate_escalations_to_model AS
-WITH cdas as (
-	SELECT distinct(census_division_abbr) as census_division_abbr
+With cdas AS (
+	SELECT distinct(census_division_abbr) as census_division_abbr, generate_series(2014,2080) as year
 	FROM diffusion_shared.county_geom
-	),
-
-esc_combined AS (
-	-- convert user defined rate projections to format consistent with diffusion_wind.rate_escalations
-	SELECT b.census_division_abbr, a.year, 'Residential'::text as sector, 
+	order by year, census_division_abbr
+),
+user_defined_gaps AS 
+(
+	SELECT b.census_division_abbr, b.year, 
 		a.user_defined_res_rate_escalations as escalation_factor,
+		lag(a.user_defined_res_rate_escalations,1) OVER (PARTITION BY b.census_division_abbr ORDER BY b.year asc) as lag_factor,
+		lead(a.user_defined_res_rate_escalations,1) OVER (PARTITION BY b.census_division_abbr ORDER BY b.year asc) as lead_factor,
+		(array_agg(a.user_defined_res_rate_escalations) OVER (PARTITION BY b.census_division_abbr ORDER BY b.year ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING))[37] as last_factor,
 		'User Defined'::text as source
-	FROM diffusion_wind.market_projections a
-	CROSS JOIN cdas b
-
-	UNION
-
-	SELECT b.census_division_abbr, a.year, 'Commercial'::text as sector, 
-		a.user_defined_com_rate_escalations as escalation_factor,
-		'User Defined'::text as source
-	FROM diffusion_wind.market_projections a
-	CROSS JOIN cdas b
-
-	UNION
-
-	SELECT b.census_division_abbr, a.year, 'Industrial'::text as sector, 
-		a.user_defined_ind_rate_escalations as escalation_factor,
-		'User Defined'::text as source
-	FROM diffusion_wind.market_projections a
-	CROSS JOIN cdas b
-
-	UNION
-	-- create No Growth projections using the same method
-	SELECT b.census_division_abbr, a.year, 'Residential'::text as sector, 
+	FROM cdas b
+	LEFT JOIN diffusion_wind.market_projections a
+       on a.year = b.year
+),
+user_defined_filled AS
+(
+	SELECT census_division_abbr, year, unnest(array['Residential','Commercial','Industrial'])::text as sector,
+	CASE WHEN escalation_factor is null and year <= 2050 THEN (lag_factor+lead_factor)/2
+	     WHEN escalation_factor is null and year > 2050 THEN last_factor
+	     ELSE escalation_factor
+	END as escation_factor,
+	source
+	FROM user_defined_gaps
+),
+no_growth AS (
+SELECT census_division_abbr, year, unnest(array['Residential','Commercial','Industrial'])::text as sector,
 		1::numeric as escalation_factor,
 		'No Growth'::text as source
-	FROM diffusion_wind.market_projections a
-	CROSS JOIN cdas b
-
-	UNION
-
-	SELECT b.census_division_abbr, a.year, 'Commercial'::text as sector, 
-		1::numeric as escalation_factor,
-		'No Growth'::text as source
-	FROM diffusion_wind.market_projections a
-	CROSS JOIN cdas b
-
-	UNION
-
-	SELECT b.census_division_abbr, a.year, 'Industrial'::text as sector, 
-		1::numeric as escalation_factor,
-		'No Growth'::text as source
-	FROM diffusion_wind.market_projections a
-	CROSS JOIN cdas b
-
-	UNION
-
-	-- add in the prestaged AEO2014 projections
-	SELECT census_division_abbr, year, sector, escalation_factor, source
+	FROM cdas
+),
+aeo AS 
+(
+	SELECT census_division_abbr, year, sector, 
+		escalation_factor, 
+		source
 	FROM diffusion_shared.rate_escalations
+	where year >= 2014
+),
+esc_combined AS
+(
+	SELECT *
+	FROM aeo
 
-	order by year, sector, census_division_abbr, source),
+	UNION 
 
-inp_opts AS (
+	SELECT *
+	FROM no_growth
+
+	UNION 
+
+	SELECT *
+	FROM user_defined_filled
+),
+inp_opts AS 
+(
 	SELECT 'Residential'::text as sector, res_rate_escalation as source
 	FROM diffusion_wind.scenario_options
 	UNION
@@ -292,7 +288,8 @@ inp_opts AS (
 	FROM diffusion_wind.scenario_options
 	UNION
 	SELECT 'Industrial'::text as sector, ind_rate_escalation as source
-	FROM diffusion_wind.scenario_options)
+	FROM diffusion_wind.scenario_options
+)
 
 SELECT a.census_division_abbr, a.year, a.sector, a.escalation_factor, a.source
 FROM esc_combined a
