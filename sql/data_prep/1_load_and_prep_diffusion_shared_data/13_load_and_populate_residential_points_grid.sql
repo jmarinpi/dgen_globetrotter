e0225f3358f10a256de5aa92cffa677f67af6ba3
mgleason <michael.gleason@nrel.gov>
2014-09-23 15:50:46 -0600

    Adding code for incorporating solar incentives from dsire

--- sql/data_prep/1_load_and_prep_diffusion_shared_data/13_load_and_populate_residential_points_grid.sql
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/13_load_and_populate_residential_points_grid.sql
@@ -516,6 +516,7 @@ where a.gid = b.gid;
 	WHERE annual_rate_gid is null;
 
 -- create dsire incentives lookup table
+	-- FOR BOTH WIND AND SOLAR
 	--create a copy of the incentives_geoms table so i can index the geom
 	CREATE TABLE dg_wind.incentives_geoms_copy AS
 	sELECT gid, the_geom
@@ -536,20 +537,20 @@ where a.gid = b.gid;
 
 	VACUUM ANALYZE dg_wind.incentives_geoms_copy_diced;
 
+	-- WIND
 	--create the lookup table		
 	DROP TABLE IF EXISTS diffusion_wind.dsire_incentives_lookup_res;
 	CREATE TABLE diffusion_wind.dsire_incentives_lookup_res AS
 
-	with a as (
-	SELECT b.gid, b.the_geom, d.uid as wind_incentives_uid
-
-	FROM dg_wind.incentives_geoms_copy_diced b
-
-	inner JOIN geo_incentives.incentives c
-	ON b.gid = c.geom_id
-
-	INNER JOIN geo_incentives.wind_incentives d
-	ON c.gid = d.incentive_id)
+	with a as 
+	(
+		SELECT b.gid, b.the_geom, d.uid as wind_incentives_uid
+		FROM dg_wind.incentives_geoms_copy_diced b
+		inner JOIN geo_incentives.incentives c
+		ON b.gid = c.geom_id
+		INNER JOIN geo_incentives.wind_incentives d
+		ON c.gid = d.incentive_id
+	)
 
 	SELECT e.gid as pt_gid, a.wind_incentives_uid
 	FROM a
@@ -626,6 +627,99 @@ where a.gid = b.gid;
 	CREATE INDEX dsire_incentives_simplified_lkup_res_inc_id_btree
 	ON diffusion_wind.dsire_incentives_simplified_lkup_res
 	USING btree(wind_incentive_array_id);
+
+	-- SOLAR
+	--create the lookup table		
+	DROP TABLE IF EXISTS diffusion_solar.dsire_incentives_lookup_res;
+	CREATE TABLE diffusion_solar.dsire_incentives_lookup_res AS
+
+	with a as 
+	(
+		SELECT b.gid, b.the_geom, d.uid as solar_incentives_uid
+		FROM dg_wind.incentives_geoms_copy_diced b
+		inner JOIN geo_incentives.incentives c
+		ON b.gid = c.geom_id
+		INNER JOIN geo_incentives.pv_incentives d
+		ON c.gid = d.incentive_id
+	)
+
+	SELECT e.gid as pt_gid, a.solar_incentives_uid
+	FROM a
+
+	INNER JOIN diffusion_shared.pt_grid_us_res e
+	ON ST_Intersects(a.the_geom,e.the_geom_4326);
+
+	CREATE INDEX dsire_incentives_lookup_res_pt_gid_btree ON diffusion_solar.dsire_incentives_lookup_res using btree(pt_gid);
+
+	SELECT pt_gid, count(*)
+	FROM diffusion_solar.dsire_incentives_lookup_res
+	GROUP BY pt_gid
+	ORDER by count desc;
+
+	-- group the incentives into arrays so that there is just one row for each pt_gid
+	DROP TABLE IF EXISTS diffusion_solar_data.dsire_incentives_combos_lookup_res;
+	CREATE TABLE diffusion_solar_data.dsire_incentives_combos_lookup_res AS
+	SELECT pt_gid, array_agg(solar_incentives_uid order by solar_incentives_uid) as solar_incentives_uid_array
+	FROM diffusion_solar.dsire_incentives_lookup_res
+	group by pt_gid;
+
+	-- find the unique set of incentive arrays
+	DROP TABLE IF EXISTS diffusion_solar_data.dsire_incentives_unique_combos_res;
+	CREATE TABLE diffusion_solar_data.dsire_incentives_unique_combos_res AS
+	SELECT distinct(solar_incentives_uid_array) as solar_incentives_uid_array
+	FROM diffusion_solar_data.dsire_incentives_combos_lookup_res;
+
+	-- add a primary key to the table of incentive arrays
+	ALTER TABLE diffusion_solar_data.dsire_incentives_unique_combos_res
+	ADD column incentive_array_id serial primary key;
+
+	-- join the incentive array primary key back into the combos_lookup_table
+	ALTER TABLE diffusion_solar_data.dsire_incentives_combos_lookup_res
+	ADD column incentive_array_id integer;
+
+	UPDATE diffusion_solar_data.dsire_incentives_combos_lookup_res a
+	SET incentive_array_id = b.incentive_array_id
+	FROM diffusion_solar_data.dsire_incentives_unique_combos_res b
+	where a.solar_incentives_uid_array = b.solar_incentives_uid_array;
+
+	-- join this info back into the main points table
+	ALTER TABLE diffusion_shared.pt_grid_us_res
+	ADD COLUMN solar_incentive_array_id integer;
+
+	UPDATE diffusion_shared.pt_grid_us_res a
+	SET solar_incentive_array_id = b.incentive_array_id
+	FROM diffusion_solar_data.dsire_incentives_combos_lookup_res b
+	WHere a.gid = b.pt_gid;
+	
+	-- add an index
+	CREATE INDEX pt_grid_us_res_solar_incentive_btree 
+	ON diffusion_shared.pt_grid_us_res
+	USING btree(solar_incentive_array_id);
+
+	-- check that we got tem all
+	SELECT count(*)
+	FROM diffusion_shared.pt_grid_us_res
+	where solar_incentive_array_id is not null;
+	--6273172
+
+	SELECT count(*)
+	FROM diffusion_solar_data.dsire_incentives_combos_lookup_res
+	where incentive_array_id is not null;
+	--6273172
+
+	--unnest the data from the unique combos table
+	DROP TABLE IF EXISTS diffusion_solar.dsire_incentives_simplified_lkup_res;
+	CREATE TABLE diffusion_solar.dsire_incentives_simplified_lkup_res AS
+	SELECT incentive_array_id as solar_incentive_array_id, 
+		unnest(solar_incentives_uid_array) as solar_incentives_uid
+	FROM diffusion_solar_data.dsire_incentives_unique_combos_res;
+
+	-- create index
+	CREATE INDEX dsire_incentives_simplified_lkup_res_inc_id_btree
+	ON diffusion_solar.dsire_incentives_simplified_lkup_res
+	USING btree(solar_incentive_array_id);	
+
+	
 
 
 -- test county level access
---
+++ sql/data_prep/3b_create_solar_point_microdata_tables.sql
@@ -0,0 +1,137 @@
+﻿-- residential
+DROP TABLE IF EXISTS diffusion_solar.point_microdata_res_us;
+SET seed to 1;
+CREATE TABLE diffusion_solar.point_microdata_res_us AS
+WITH a AS
+(
+	SELECT a.county_id, 
+		a.annual_rate_gid, 
+		'p'::text || a.pca_reg::text AS pca_reg, a.reeds_reg, a.solar_incentive_array_id,
+		a.utility_type, 
+		a.solar_re_9809_gid,
+		count(*)::integer as point_weight
+	FROM diffusion_shared.pt_grid_us_res a
+	GROUP BY a.county_id, 
+		a.annual_rate_gid, 
+		a.pca_reg, a.reeds_reg, a.wind_incentive_array_id,
+		a.utility_type, 
+		a.solar_re_9809_gid
+)
+SELECT (row_number() OVER (ORDER BY county_id, random()))::integer as micro_id, *
+FROM a
+ORDER BY county_id;
+--use setseed() and order by random() as a secondary sort key to ensure order will be the same if we have to re run
+
+
+-- primary key and indices
+ALTER TABLE diffusion_solar.point_microdata_res_us
+ADD primary key (micro_id);
+
+CREATE INDEX point_microdata_res_us_county_id_btree
+  ON diffusion_solar.point_microdata_res_us
+  USING btree (county_id);
+
+CREATE INDEX point_microdata_res_us_annual_rate_gid_btree
+  ON diffusion_solar.point_microdata_res_us
+  USING btree (annual_rate_gid);
+
+CREATE INDEX point_microdata_res_us_utility_type_btree
+  ON diffusion_solar.point_microdata_res_us
+  USING btree (utility_type);
+
+BEGIN;
+VACUUM ANALYZE diffusion_solar.point_microdata_res_us;
+commit;
+
+----------------------------------------------------------------------------------------------------
+-- commercial
+DROP TABLE IF EXISTS diffusion_solar.point_microdata_com_us;
+SET seed to 1;
+CREATE TABLE diffusion_solar.point_microdata_com_us AS
+WITH a AS
+(
+	SELECT a.county_id, 
+		a.annual_rate_gid, 
+		'p'::text || a.pca_reg::text AS pca_reg, a.reeds_reg, a.solar_incentive_array_id,
+		a.utility_type, 
+		a.solar_re_9809_gid,
+		count(*)::integer as point_weight
+	FROM diffusion_shared.pt_grid_us_com a
+	GROUP BY a.county_id, 
+		a.annual_rate_gid, 
+		a.pca_reg, a.reeds_reg, a.solar_incentive_array_id,
+		a.utility_type, 
+		a.solar_re_9809_gid
+)
+SELECT (row_number() OVER (ORDER BY county_id, random()))::integer as micro_id, *
+FROM a
+ORDER BY county_id;
+--use setseed() and order by random() as a secondary sort key to ensure order will be the same if we have to re run
+
+
+-- primary key and indices
+ALTER TABLE diffusion_solar.point_microdata_com_us
+ADD primary key (micro_id);
+
+CREATE INDEX point_microdata_com_us_county_id_btree
+  ON diffusion_solar.point_microdata_com_us
+  USING btree (county_id);
+
+CREATE INDEX point_microdata_com_us_annual_rate_gid_btree
+  ON diffusion_solar.point_microdata_com_us
+  USING btree (annual_rate_gid);
+
+CREATE INDEX point_microdata_com_us_utility_type_btree
+  ON diffusion_solar.point_microdata_com_us
+  USING btree (utility_type);
+
+BEGin;
+VACUUM ANALYZE diffusion_solar.point_microdata_com_us;
+commit;
+
+
+----------------------------------------------------------------------------------------------------
+-- industrial
+DROP TABLE IF EXISTS diffusion_solar.point_microdata_ind_us;
+SET seed to 1;
+CREATE TABLE diffusion_solar.point_microdata_ind_us AS
+WITH a AS
+(
+	SELECT a.county_id, 
+		a.annual_rate_gid, 
+		'p'::text || a.pca_reg::text AS pca_reg, a.reeds_reg, a.solar_incentive_array_id,
+		a.utility_type, 
+		a.solar_re_9809_gid,
+		count(*)::integer as point_weight
+	FROM diffusion_shared.pt_grid_us_ind a
+	GROUP BY a.county_id, 
+		a.annual_rate_gid, 
+		a.pca_reg, a.reeds_reg, a.solar_incentive_array_id,
+		a.utility_type, 
+		a.solar_re_9809_gid
+)
+SELECT (row_number() OVER (ORDER BY county_id, random()))::integer as micro_id, *
+FROM a
+ORDER BY county_id;
+--use setseed() and order by random() as a secondary sort key to ensure order will be the same if we have to re run
+
+
+-- primary key and indices
+ALTER TABLE diffusion_solar.point_microdata_ind_us
+ADD primary key (micro_id);
+
+CREATE INDEX point_microdata_ind_us_county_id_btree
+  ON diffusion_solar.point_microdata_ind_us
+  USING btree (county_id);
+
+CREATE INDEX point_microdata_ind_us_annual_rate_gid_btree
+  ON diffusion_solar.point_microdata_ind_us
+  USING btree (annual_rate_gid);
+
+CREATE INDEX point_microdata_ind_us_utility_type_btree
+  ON diffusion_solar.point_microdata_ind_us
+  USING btree (utility_type);
+
+Begin;
+VACUUM ANALYZE diffusion_solar.point_microdata_ind_us;
+commit;
