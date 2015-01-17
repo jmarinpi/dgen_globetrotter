5b51cf063737fb9176af1ea0bd42593f8699760e
mgleason <michael.gleason@nrel.gov>
2015-01-16 17:23:36 -0700

    Adding function to add or update key in json in postgres

---
+++ sql/functions/add_key_to_json.sql
@@ -0,0 +1,22 @@
+﻿
+DROP FUNCTION IF EXISTS public.add_key(j json, k text, value integer[]);
+SET ROLE 'server-superusers';
+CREATE OR REPLACE FUNCTION public.add_key(j json, k text, value integer[])
+  RETURNS json AS
+  $BODY$
+
+	import json
+	d = json.loads(j)
+	if k in d.keys():
+		del d[k]
+	else:
+		plpy.warning("KeyError: Key '%s' does not exist in json" % k) 
+	s = json.dumps(d)
+
+	return s
+	
+
+  $BODY$
+  LANGUAGE plpythonu stable
+  COST 100;
+RESET ROLE;
