260d4986fca9a9f3195304347f59298c19fc7570
mgleason <michael.gleason@nrel.gov>
2015-02-04 08:59:29 -0700

    Adding code to estimate roof areas for cbecs

--- sql/data_prep/1_load_and_prep_diffusion_shared_data/16e_estimate_rooftop_area_in_recs_cbecs.sql
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/16e_estimate_rooftop_area_in_recs_cbecs.sql
@@ -76,3 +76,67 @@ order by 1;
 ---------------------------------------------------------------------------------------------------
 
 -- CBECS
+-- check the proportion of buildings with different nfloors
+select nfloor8, count(*), sum(adjwt8)
+FROM diffusion_shared.eia_microdata_cbecs_2003
+where pba8 <> 1
+group by nfloor8
+order by nfloor8;
+-- nfloor = 1-14 are literal floor counts
+-- 991 = 15-25 floors
+-- 992 = >25 floors
+-- recode as follows:
+-- 991 = 20
+-- 992 = 30
+
+-- why are there nulls?
+select distinct(crb_model)
+FROM diffusion_shared.eia_microdata_cbecs_2003
+where pba8 <> 1
+and nfloor8 is null;
+-- all strip malls
+
+-- add a column for the roof area in sqft
+ALTER TABLE diffusion_shared.eia_microdata_cbecs_2003
+ADD COLUMN roof_sqft integer;
+
+-- assume all strip malls are single floor
+
+with a as
+(
+	select *, 
+		case 	when nfloor8 = 991 then 20
+			when nfloor8 = 992 then 30
+			when nfloor8 is null then 1
+		else nfloor8
+		end as num_floors
+	from diffusion_shared.eia_microdata_cbecs_2003
+	where pba8 <> 1
+),
+b AS
+(
+	select pubid8, sqft8/num_floors as roof_sqft
+	from a
+)
+UPDATE diffusion_shared.eia_microdata_cbecs_2003 c
+set roof_sqft = b.roof_sqft
+from b
+where c.pubid8 = b.pubid8
+
+
+
+-- make sure no nulls
+select count(*)
+from diffusion_shared.eia_microdata_cbecs_2003
+where pba8 <> 1
+and roof_sqft is null;
+-- zero --  all set
+
+-- check results
+select roof_sqft, *
+from diffusion_shared.eia_microdata_cbecs_2003
+where roof_sqft is not null
+order by 1;
+-- there are some very roof areas but they all appear to be single story buildings with large sq ft,
+-- so unless the stories field is coded incorrectly, the algebra should be correct
+
