08cfca243a7a9ec41efe6169b81f9f5165dceb09
mgleason <michael.gleason@nrel.gov>
2015-02-05 16:49:02 -0700

    Working on new com/ind land masks and associated documentation

--- sql/data_prep/1_load_and_prep_diffusion_shared_data/2_calculate_and_backfill_county_annual_electric_load/3_commercial_load/1_create_commercial_land_mask/2_isolate_known_commercial_facilities.sql
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/2_calculate_and_backfill_county_annual_electric_load/3_commercial_load/1_create_commercial_land_mask/2_isolate_known_commercial_facilities.sql
@@ -1,4 +1,4 @@
--- isolate commercial points from navteq based on fac_types that map to cbecs
+﻿-- isolate commercial points from navteq based on fac_types that map to cbecs
 CREATE OR REPLACE VIEW dg_wind.navteq_commercial_facilities AS
 SELECT a.feature, b.*
 FROM dg_wind.navteq_fac_type_to_cbecs_pba a
@@ -33,37 +33,38 @@ ALTER TABLE dg_wind.hsip_and_navteq_commercial_facilities ADD COLUMN gid serial;
 CREATE INDEX hsip_and_navteq_commercial_facilities_the_geom_4326_gist ON dg_wind.hsip_and_navteq_commercial_facilities USING gist(the_geom_4326);
 VACUUM ANALYZE dg_wind.hsip_and_navteq_commercial_facilities;
 
--- extract daytime/nighttime pop ratio to points
-DROP TABLE IF EXISTS dg_wind.hsip_and_navteq_commercial_pts_conus_pop_ratio_nightday;
-CREATE TABLE dg_wind.hsip_and_navteq_commercial_pts_conus_pop_ratio_nightday (
-	gid integer,
-	pop_ratio_nightday float);
-
-	--run in parallel for speed (100x100 tiles are necessary for it ti finishin in about 7 mins -- 1000x1000 tiles would take several hours even in parallel)
-	SELECT parsel_2('dav-gis','dg_wind.hsip_and_navteq_commercial_pts','gid',
-			'SELECT a.gid, ST_Value(b.rast,a.the_geom_4326) as pop_ratio_daynight
-			FROM  dg_wind.hsip_and_navteq_commercial_pts a
-			INNER JOIN dg_wind.ls2012_popratio_nightday_us_100x100 b
-			ON ST_Intersects(b.rast,a.the_geom_4326)
-			WHERE ST_GeometryType(a.the_geom_4326) = ''ST_Point'';',
-		'dg_wind.hsip_and_navteq_commercial_pts_conus_pop_ratio_nightday', 'a',16);
-
--- review results in R: use check_daynight_popratio_for_commercial_points.R, and in arcgis -- see where to set the threshold for comm facilities
--- use 
-
 -- buffer the points 
-CREATE TABLE dg_wind.hsip_and_navteq_commercial_facility_buffers AS
-SELECT gid,
-	CASE WHEN ST_GeometryType(the_geom_4326) = 'ST_Point' THEN ST_Buffer(the_geom_4326::geography, 90)::geometry
-	else the_geom_4326
-	END as the_geom_4326, 
-	source, table_name, table_number
-FROM dg_wind.hsip_and_navteq_commercial_facilities;
---4835919 rows
+DROP TABLE IF EXISTS dg_wind.hsip_and_navteq_commercial_facility_buffers CASCADE;
+CREATE TABLE dg_wind.hsip_and_navteq_commercial_facility_buffers
+(
+  gid integer,
+  the_geom_4326 geometry,
+  source text,
+  table_name text,
+  table_number integer
+);
 
--- make sure all the data is polygon data now
+SELECT parsel_2('dav-gis','mgleason','mgleason','dg_wind.hsip_and_navteq_commercial_facilities','gid',
+	'SELECT a.gid,
+		CASE WHEN ST_GeometryType(a.the_geom_4326) = ''ST_Point'' THEN ST_Buffer(a.the_geom_4326::geography, 200)::geometry
+		else a.the_geom_4326
+		END as the_geom_4326, 
+		a.source, a.table_name, a.table_number
+	FROM dg_wind.hsip_and_navteq_commercial_facilities a;',
+		'dg_wind.hsip_and_navteq_commercial_facility_buffers', 'a',16);
+
+-- check count
+select count(*)
+FROM dg_wind.hsip_and_navteq_commercial_facility_buffers;
+-- 4835919
+
+select count(*)
+from dg_wind.hsip_and_navteq_commercial_facilities;
+-- 4835919 (all set)
+		
+-- make sure all the data is polygon or multipolygon data now
 SELECT distinct(ST_GeometryType(the_geom_4326))
-FROM dg_wind.hsip_and_navteq_commercial_facility_buffers
+FROM dg_wind.hsip_and_navteq_commercial_facility_buffers;
 
 -- this is too big to export to shapefile all at once, so create three views that split the data into pieces:
 SELECT min(gid),max(gid) from dg_wind.hsip_and_navteq_commercial_facility_buffers;
@@ -85,13 +86,15 @@ SELECT *
 FROM dg_wind.hsip_and_navteq_commercial_facility_buffers
 where gid >= 3223946;
 
--- export to shapefiles --> S:\mgleason\DG_Wind\Data\Analysis\commercial_land_mask\commercial_facility_polygons\hsip_and_navteq_commercial_facility_buffers_part1.shp, hsip_and_navteq_commercial_facility_buffers_part2.shp, hsip_and_navteq_commercial_facility_buffers_part3.shp
--- in arc, merge into a single feature class in a geodatatabase -->F:\data\mgleason\DG_Wind\Data\Analysis\commercial_land_mask\commercial_facility_polygons\commercial_facs.gdb\commercial_facilities_combined
--- convert the commercial facilities shapefile to boolean (presence of commercial = 1)
+-- export to shapefiles --> F:\data\mgleason\DG_Wind\Data\Analysis\commercial_land_mask\revised_2014_02_05\commercial_facility_polygons
+	-- hsip_and_navteq_commercial_facility_buffers_part1.shp
+	-- hsip_and_navteq_commercial_facility_buffers_part2.shp
+	-- hsip_and_navteq_commercial_facility_buffers_part3.shp
 
--- also in arc, threshold the nighttime/daytime pop ratio grids ( <=1.05 --> 1 (is commercial) ) for conus, us , and ak
--- combine the two boolean grids using boolean-or logic (if commercial in either, it is commercial)
--- resample the grid to 200m grid consistent with the AWS gcf grids
+-- in arc, merge into a single feature class in a geodatatabase 
+	-->F:\data\mgleason\DG_Wind\Data\Analysis\commercial_land_mask\revised_2014_02_05\commercial_facility_polygons\commercial_facs.gdb\commercial_facilities_combined
+
+-- convert the commercial facilities shapefile to raster snapped to aws 2014 gcf raster (presence of industrial = 1, all else = no data)
 
 -- convert this grid to points, then load those points to postgres as commercial points
 -- load this grid to postgres and use to resample load to county level
