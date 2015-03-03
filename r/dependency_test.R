17ead07f8517c343883432542f394a6a4222245e
mgleason <michael.gleason@nrel.gov>
2015-03-03 13:27:17 -0700

    adding dependency_test script for R packages

---
+++ r/dependency_test.R
@@ -0,0 +1,26 @@
+packages = c('ggthemes',
+             'httr',
+             'devtools',
+             'classInt',
+             'data.table',
+             'rjson',
+             'dplyr',
+             'devtools',
+             'rCharts',
+             'rMaps',
+             'RPostgreSQL')
+
+error = F
+for (package in packages){
+  installed = suppressMessages(require(package, quietly = T, character.only = T, warn.conflicts = F))
+  if (!installed){
+    print(sprintf('Error: %s is not installed.', package))
+    error = T
+  }
+}
+
+if (!error){
+  'Success. All R dependencies are loaded.'
+} else {
+  print('Failure. One or more R dependncies are missing.')
+}
