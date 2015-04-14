21ad22d3794bb1c14a0f0d3aaec8f35b316c3220
Jon Duckworth <Jonathan.Duckworth@nrel.gov>
2015-04-14 15:13:34 -0600

    SQL to create combined cbecs/recs view, Issue #309

---
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/16_ingest_eia_microdata/6_combine_recs_cbecs.sql
@@ -0,0 +1,39 @@
+﻿SET ROLE 'diffusion-writers';
+DROP VIEW IF EXISTS diffusion_shared.cbecs_recs_combined;
+
+CREATE OR REPLACE VIEW diffusion_shared.cbecs_recs_combined
+AS
+SELECT c.pubid8 as load_id,
+c.adjwt8 as weight,
+c.elcns8 as ann_cons_kwh,
+c.crb_model as crb_model,
+c.roof_style as roof_style,
+c.roof_sqft as roof_sqft,
+c.ownocc8 as ownocc8,
+'diffusion_shared.eia_microdata_cbecs_2003' as source_table
+FROM diffusion_shared.eia_microdata_cbecs_2003 as c
+UNION
+SELECT r.doeid as load_id,
+r.nweight as weight,
+r.kwh as ann_cons_kwh,
+r.crb_model as crb_model,
+r.roof_style as roof_style,
+r.roof_sqft as roof_sqft,
+1 as ownocc8,
+'diffusion_shared.eia_microdata_recs_2009' as source_table
+FROM diffusion_shared.eia_microdata_recs_2009 as r
+;
+
+COMMENT ON VIEW diffusion_shared.cbecs_recs_combined IS '''
+Combined data from eia_microdata_cbecs_2003 and eia_microdata_recs_2009 with standardized field names. 
+See column-level comments for relationship to original field names
+''';
+
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.load_id IS 'recs.doeid or cbecs.pubid8';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.weight IS 'recs.nweight or cbecs.adjwt8';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.ann_cons_kwh IS 'recs.kwh or cbecs.elcns8';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.crb_model IS 'recs.crb_model or cbecs.crb_model';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.roof_style IS 'recs.roof_style or cbecs.roof_style';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.roof_sqft IS 'recs.roof_sqft or cbecs.roof_sqft';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.ownocc8 IS 'recs.ownocc8 or 1 for cbecs';
+COMMENT ON COLUMN diffusion_shared.cbecs_recs_combined.source_table IS 'recs or cbecs source table';
