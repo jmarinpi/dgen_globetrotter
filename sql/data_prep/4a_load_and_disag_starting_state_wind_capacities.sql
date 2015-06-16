3d6539a6b0474c8b5c6198b7092b51ee07c6a989
mgleason <michael.gleason@nrel.gov>
2015-06-16 16:33:50 -0600

    working on #343. wind and solar starting capacities are now disaggregated directly from state x sector totals to customer bins dynamically in each model run

--- sql/data_prep/4a_load_and_disag_starting_state_wind_capacities.sql
+++ sql/data_prep/4a_load_and_disag_starting_state_wind_capacities.sql
@@ -1,4 +1,6 @@
-﻿CREATE TABLE dg_wind.existing_distributed_wind_capacity_by_state_2014 (
+﻿set role 'diffusion-writers';
+
+CREATE TABLE dg_wind.existing_distributed_wind_capacity_by_state_2014 (
 	state text,
 	sector text,
 	capacity_mw numeric);
@@ -29,78 +31,49 @@ set (state,state_abbr) = ('Puerto Rico-Virgin Islands','PR-VI')
 where state = 'PR-VI';
 
 
--- disaggregate to counties based on proprtion of county load to proportion of state load
-DROP TABLE IF EXISTS diffusion_wind.starting_capacities_mw_2014_us;
+-- backup old table
+-- DROP TABLE IF EXIStS diffusion_wind.starting_capacities_mw_2014_us_backup;
+-- CREATE TABLE diffusion_wind.starting_capacities_mw_2014_us_backup AS
+-- SELECT *
+-- FROM diffusion_wind.starting_capacities_mw_2014_us;
+
+-- cast to wide format and move to diffusion_wind schema
+DROP TABLE IF EXIStS diffusion_wind.starting_capacities_mw_2014_us;
 CREATE TABLE diffusion_wind.starting_capacities_mw_2014_us AS
-with sums as (
-	SELECT a.state_abbr, 
-		sum(b.total_customers_2011_residential) as state_customers_residential, 
-		sum(b.total_customers_2011_commercial) as state_customers_commercial, 
-		sum(b.total_customers_2011_industrial) as state_customers_industrial
-	FROM diffusion_shared.county_geom a
-	LEFT JOIN diffusion_shared.load_and_customers_by_county_us b
-	ON a.county_id = b.county_id
-	where a.state_abbr not in ('AK','HI')
-	GROUP BY a.state_abbr
-	),
-counties as (
-	SELECT a.state_abbr, a.county_id,
-		b.total_customers_2011_residential,
-		b.total_customers_2011_commercial,
-		b.total_customers_2011_industrial
-	FROM diffusion_shared.county_geom a
-	LEFT JOIN diffusion_shared.load_and_customers_by_county_us b
-	ON a.county_id = b.county_id
-	where a.state_abbr not in ('AK','HI')
-	),
-capacities as (
-	SELECT a.state_abbr, 
-		a.capacity_mw as capacity_mw_residential,
-		b.capacity_mw as capacity_mw_commercial,
-		c.capacity_mw as capacity_mw_industrial,
-
-		a.systems_count as systems_count_residential,
-		b.systems_count as systems_count_commercial,
-		c.systems_count as systems_count_industrial
-				
-	FROM dg_wind.existing_distributed_wind_capacity_by_state_2014 a
-	left join dg_wind.existing_distributed_wind_capacity_by_state_2014 b
-	ON a.state_abbr = b.state_abbr
-	left join dg_wind.existing_distributed_wind_capacity_by_state_2014 c
-	ON a.state_abbr = c.state_abbr
-	where a.sector = 'Residential'
-	and b.sector = 'Commercial'
-	and c.sector = 'Industrial')
-	
-SELECT a.state_abbr, a.county_id, 
-	a.total_customers_2011_residential/b.state_customers_residential*c.capacity_mw_residential as capacity_mw_residential,
-	a.total_customers_2011_commercial/b.state_customers_commercial*c.capacity_mw_commercial as capacity_mw_commercial,
-	a.total_customers_2011_industrial/b.state_customers_industrial*c.capacity_mw_industrial as capacity_mw_industrial,
-
-	a.total_customers_2011_residential/b.state_customers_residential*c.systems_count_residential as systems_count_residential,
-	a.total_customers_2011_commercial/b.state_customers_commercial*c.systems_count_commercial as systems_count_commercial,
-	a.total_customers_2011_industrial/b.state_customers_industrial*c.systems_count_industrial as systems_count_industrial
-	
-FROM counties a
-LEFT JOIN sums b
-ON a.state_abbr = b.state_abbr
-LEFT JOIN capacities c
+with a as
+(
+	select state_abbr, capacity_mw as capacity_mw_residential, systems_count as systems_count_residential
+	FROM dg_wind.existing_distributed_wind_capacity_by_state_2014
+	where sector = 'Residential'
+),
+b as
+(
+	select state_abbr, capacity_mw as capacity_mw_commercial, systems_count as systems_count_commercial
+	FROM dg_wind.existing_distributed_wind_capacity_by_state_2014
+	where sector = 'Commercial'
+),
+c as
+(
+	select state_abbr, capacity_mw as capacity_mw_industrial, systems_count as systems_count_industrial
+	FROM dg_wind.existing_distributed_wind_capacity_by_state_2014
+	where sector = 'Industrial'
+)
+select a.state_abbr, 
+	a.capacity_mw_residential, 
+	b.capacity_mw_commercial, 
+	c.capacity_mw_industrial, 
+	a.systems_count_residential, 
+	b.systems_count_commercial, 
+	c.systems_count_industrial
+from a
+left join b
+on a.state_abbr = b.state_abbr
+LEFT JOIN c
 ON a.state_abbr = c.state_abbr;
+
+
 
 -- create primary key and foreign key
 ALTER TABLE diffusion_wind.starting_capacities_mw_2014_us
-  ADD CONSTRAINT starting_capacities_mw_2014_us_pkey PRIMARY KEY(county_id);
+  ADD CONSTRAINT starting_capacities_mw_2014_us_pkey PRIMARY KEY(state_abbr);
 
-ALTER TABLE diffusion_wind.starting_capacities_mw_2014_us
-  ADD CONSTRAINT county_id FOREIGN KEY (county_id)
-      REFERENCES diffusion_shared.county_geom (county_id) MATCH FULL
-      ON UPDATE RESTRICT ON DELETE RESTRICT;
-
-
--- check results
-select state_abbr, 
-	round(sum(capacity_mw_residential),2) as res_cap, round(sum(capacity_mw_commercial),2) com_cap, round(sum(capacity_mw_industrial),2) ind_cap,
-	round(sum(systems_count_residential),2) res_sys, round(sum(systems_count_commercial),2) com_sys, round(sum(systems_count_industrial),2) ind_sys
-FROM diffusion_wind.starting_capacities_mw_2014_us
-group by state_abbr
-order by state_abbr;
