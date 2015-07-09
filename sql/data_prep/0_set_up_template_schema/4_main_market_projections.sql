9163dba48f1415a1ab5aecb2a2fba9ba4c35d005
mgleason <michael.gleason@nrel.gov>
2015-07-09 16:19:31 -0600

    more progress on mapping named ranges to postgres tables

--- sql/data_prep/0_set_up_template_schema/4_main_market_projections.sql
+++ sql/data_prep/0_set_up_template_schema/4_main_market_projections.sql
@@ -11,23 +11,23 @@ CREATE TABLE diffusion_template.input_main_inflation
 DROP TABLE if exists diffusion_template.input_main_market_projections;
 CREATE TABLE diffusion_template.input_main_market_projections
 (
-	year integer,
-	avoided_costs_dollars_per_kwh numeric,
-	carbon_dollars_per_ton numeric,
-	user_defined_res_rate_escalations numeric,
-	user_defined_com_rate_escalations numeric,
-	user_defined_ind_rate_escalations numeric,
-	default_rate_escalations numeric
+	year integer NOT NULL,
+	avoided_costs_dollars_per_kwh numeric NOT NULL,
+	carbon_dollars_per_ton numeric NOT NULL,
+	user_defined_res_rate_escalations numeric NOT NULL,
+	user_defined_com_rate_escalations numeric NOT NULL,
+	user_defined_ind_rate_escalations numeric NOT NULL,
+	default_rate_escalations numeric NOT NULL
 );
 
 
 DROP TABLE if exists diffusion_template.input_main_flat_electric_rates_raw;
 CREATE TABLE diffusion_template.input_main_flat_electric_rates_raw
 (
-  state_abbr character varying(2),
-  res_rate_dlrs_per_kwh numeric,
-  com_rate_dlrs_per_kwh numeric,
-  ind_rate_dlrs_per_kwh numeric
+  state_abbr character varying(2) NOT NULL,
+  res_rate_dlrs_per_kwh numeric NOT NULL,
+  com_rate_dlrs_per_kwh numeric NOT NULL,
+  ind_rate_dlrs_per_kwh numeric NOT NULL
 );
 
 DROP VIEW IF EXISTS diffusion_template.input_main_flat_electric_rates;
@@ -37,3 +37,19 @@ FROM diffusion_template.input_main_flat_electric_rates_raw a
 LEFT JOIN diffusion_shared.state_fips_lkup b
 	ON a.state_abbr = b.state_abbr;
 
+
+DROP TABLE IF EXISTS diffusion_template.input_main_rate_type_weights_raw CASCADE;
+CREATE TABLE diffusion_template.input_main_rate_type_weights_raw
+(
+	rate_type_desc text  NOT NULL,
+	res_weight numeric NOT NULL,
+	com_ind_weight numeric NOT NULL
+);
+
+DROP VIEW IF EXISTS diffusion_template.input_main_rate_type_weights;
+CREATE VIEW diffusion_template.input_main_rate_type_weights AS
+Select b.rate_type, a.rate_type_desc, 
+	a.res_weight, a.com_ind_weight as com_weight, a.com_ind_weight as ind_weight
+from diffusion_template.input_main_rate_type_weights_raw a
+LEFT JOIN diffusion_shared.rate_type_desc_lkup b
+ON a.rate_type_desc = b.rate_type_desc;
--- sql/data_prep/0_set_up_template_schema/_test_ingestion.sql
+++ sql/data_prep/0_set_up_template_schema/_test_ingestion.sql
@@ -1,1 +1,35 @@
-﻿
+﻿select *
+from diffusion_template.input_main_scenario_options;
+
+
+select *
+FROM diffusion_template.input_main_inflation;
+
+
+select *
+FROM diffusion_template.input_main_market_projections
+order by 1;
+
+-- select *
+-- FROM diffusion_solar.market_projections
+-- order by 1;
+
+
+select *
+from diffusion_template.input_main_flat_electric_rates
+order by 1;
+
+-- select *
+-- from diffusion_solar.user_defined_electric_rates
+-- order by 1;
+
+
+select *
+from diffusion_template.input_main_rate_type_weights
+order by 1;
+
+-- select *
+-- from diffusion_solar.rate_type_weights
+-- order by 1;
+
+
