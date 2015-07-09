15774919fd3509a1a3063f277798858ef11d5463
mgleason <michael.gleason@nrel.gov>
2015-07-09 15:11:40 -0600

    working on ingestinog market projections

--- sql/data_prep/0_set_up_template_schema/4_main_market_projections.sql
+++ sql/data_prep/0_set_up_template_schema/4_main_market_projections.sql
@@ -7,4 +7,16 @@ CREATE TABLE diffusion_template.input_main_inflation
 	ann_inflation numeric NOT NULL
 );
 
--- add more here...
+
+DROP TABLE if exists diffusion_template.input_main_market_projections;
+CREATE TABLE diffusion_template.input_main_market_projections
+(
+	year integer,
+	avoided_costs_dollars_per_kwh numeric,
+	carbon_dollars_per_ton numeric,
+	user_defined_res_rate_escalations numeric,
+	user_defined_com_rate_escalations numeric,
+	user_defined_ind_rate_escalations numeric,
+	default_rate_escalations numeric
+);
+
