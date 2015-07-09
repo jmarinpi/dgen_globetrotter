e1d6a9d76fe9df14d51db1bc8aa74af3b85a5b74
mgleason <michael.gleason@nrel.gov>
2015-07-09 14:59:44 -0600

    more sql file reorg

---
+++ sql/data_prep/0_set_up_template_schema/4_main_market_projections.sql
@@ -0,0 +1,10 @@
+﻿set role 'diffusion-writers';
+
+
+DROP TABLE IF EXISTS diffusion_template.input_main_inflation;
+CREATE TABLE diffusion_template.input_main_inflation
+(
+	ann_inflation numeric NOT NULL
+);
+
+-- add more here...
---
+++ sql/data_prep/0_set_up_template_schema/999_incentives_solar_and_wind.sql
@@ -0,0 +1,44 @@
+﻿set role 'diffusion-writers';
+
+-- incentives (solar and wind)
+
+DROP TABLE IF EXISTS diffusion_template.input_wind_incentive_utility_types;
+CREATE TABLE diffusion_template.input_wind_incentive_utility_types
+(
+	utility_type_iou boolean NOT NULL,
+	utility_type_muni boolean NOT NULL,
+	utility_type_coop boolean NOT NULL,
+	utility_type_allother boolean NOT NULL
+);
+
+
+DROP TABLE IF EXISTS diffusion_template.input_wind_incentive_options;
+CREATE TABLE diffusion_template.input_wind_incentive_options
+(
+	overwrite_exist_inc boolean NOT NULL,
+	incentive_start_year integer NOT NULL,
+	CONSTRAINT input_wind_incentive_options_incentive_start_year_fkey FOREIGN KEY (incentive_start_year)
+		REFERENCES diffusion_config.sceninp_year_range (val) MATCH SIMPLE
+		ON DELETE RESTRICT
+);
+
+
+DROP TABLE IF EXISTS diffusion_template.input_solar_incentive_utility_types;
+CREATE TABLE diffusion_template.input_solar_incentive_utility_types
+(
+	utility_type_iou boolean NOT NULL,
+	utility_type_muni boolean NOT NULL,
+	utility_type_coop boolean NOT NULL,
+	utility_type_allother boolean NOT NULL
+);
+
+
+DROP TABLE IF EXISTS diffusion_template.input_solar_incentive_options;
+CREATE TABLE diffusion_template.input_solar_incentive_options
+(
+	overwrite_exist_inc boolean NOT NULL,
+	incentive_start_year integer NOT NULL,
+	CONSTRAINT input_solar_incentive_options_incentive_start_year_fkey FOREIGN KEY (incentive_start_year)
+		REFERENCES diffusion_config.sceninp_year_range (val) MATCH SIMPLE
+		ON DELETE RESTRICT
+);
