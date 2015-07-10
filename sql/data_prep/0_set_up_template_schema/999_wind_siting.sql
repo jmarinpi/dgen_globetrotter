24859a9c963db8fb4683f6710f23f8dcdc67380b
mgleason <michael.gleason@nrel.gov>
2015-07-10 17:33:44 -0600

    deleting outdated sql scripts

--- sql/data_prep/0_set_up_template_schema/999_wind_siting.sql
+++
@@ -1,9 +0,0 @@
-﻿set role 'diffusion-writers';
-
-DROP TABLE IF EXISTS diffusion_template.input_wind_siting_options;
-CREATE TABLE diffusion_template.input_wind_siting_options
-(	
-	parcel_size_enabled boolean NOT NULL,
-	hi_dev_enabled boolean NOT NULL,
-	canopy_clearance_enabled boolean NOT NULL
-);
