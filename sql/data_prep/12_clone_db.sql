cf290496c979c77d2b288d83b0c22c12bb8b242b
mgleason <michael.gleason@nrel.gov>
2015-10-06 14:51:29 -0600

    editing clone db script for new database setup

--- sql/data_prep/12_clone_db.sql
+++ sql/data_prep/12_clone_db.sql
@@ -1,14 +1,14 @@
-﻿set role 'server-superusers';
-DROP DATABASE IF EXISTS "diffusion_clone";
-CREATE DATABASE "diffusion_clone";
-ALTER DATABASe diffusion_clone with CONNECTION LIMIT 100;
-GRANT ALL ON DATABASE "diffusion_clone" TO "server-superusers" WITH GRANT OPTION;
-GRANT CONNECT, TEMPORARY ON DATABASE "diffusion_clone" TO public;
-GRANT ALL ON DATABASE "diffusion_clone" TO "server-creators";
-GRANT ALL ON DATABASE "diffusion_clone" TO postgres WITH GRANT OPTION;
-GRANT CONNECT, TEMPORARY ON DATABASE "diffusion_clone" TO "diffusion-writers";
-GRANT CONNECT, TEMPORARY ON DATABASE "diffusion_clone" TO "diffusion_shared-writers";
-GRANT CONNECT, TEMPORARY ON DATABASE "diffusion_clone" TO "wind_ds-writers";
+﻿-- set role 'server-superusers';
+DROP DATABASE IF EXISTS "dgen_db";
+CREATE DATABASE "dgen_db";
+ALTER DATABASe dgen_db with CONNECTION LIMIT 100;
+GRANT ALL ON DATABASE "dgen_db" TO "diffusion-admins" WITH GRANT OPTION;
+GRANT CONNECT, TEMPORARY ON DATABASE "dgen_db" TO public;
+GRANT ALL ON DATABASE "dgen_db" TO postgres WITH GRANT OPTION;
+GRANT CONNECT, TEMPORARY ON DATABASE "dgen_db" TO "diffusion-writers";
+GRANT CONNECT, TEMPORARY ON DATABASE "dgen_db" TO "diffusion-schema-writers";
+GRANT CONNECT, TEMPORARY ON DATABASE "dgen_db" TO "diffusion-intermediate";
+ALTER DATABASE dgen_db owner to "diffusion-admins";
 
 -- *** -- *** -- *** -- *** -- *** -- *** -- *** -- *** -- *** -- *** -- *** 
 -- CHANGE DATABASE CONNECTION MANUALLY BEFORE PROCEEDING!!!!!!
@@ -31,39 +31,37 @@ CREAte extension plpythonu;
 
 -- clone the schemas that we need to the new database
 -- ssh to gispgdb, then:
--- pg_dump -h localhost -U mgleason -O -n diffusion_shared -n geo_incentives -n urdb_rates -n diffusion_wind -n diffusion_wind_config -n diffusion_solar -n diffusion_solar_config dav-gis | psql -h localhost -U mgleason diffusion_clone
--- pg_dump -h localhost -U mgleason -O -n diffusion_shared -n geo_incentives -n urdb_rates -n diffusion_wind -n diffusion_wind_config -n diffusion_solar -n diffusion_solar_config diffusion_clone | psql -h dnpdb001.bigde.nrel.gov -p 5433 -U mgleason_su diffusion
--- to new server:
--- pg_dump -h localhost -U mgleason -O diffusion_clone_4 | psql -h dnpdb001.bigde.nrel.gov -p 5433 -U mgleason_su diffusion
+-- pg_dump -h localhost -U mgleason -O -n diffusion_config -n diffusion_shared -n diffusion_wind -n diffusion_solar -n diffusion_template -n urdb_rates -v dav-gis  | psql -h dnpdb001.bigde.nrel.gov -p 5433 -U mgleason_su -e dgen_db
+
 
 -- set ownership in all schemas, tables, views, and sequences
 ALTER SCHEMA diffusion_shared owner to "diffusion-writers";
 ALTER SCHEMA diffusion_solar owner to "diffusion-writers";
-ALTER SCHEMA diffusion_solar_config owner to "diffusion-writers";
+ALTER SCHEMA diffusion_config owner to "diffusion-writers";
 ALTER SCHEMA diffusion_wind owner to "diffusion-writers";
-ALTER SCHEMA diffusion_wind_config owner to "diffusion-writers";
-ALTER SCHEMA geo_incentives owner to "diffusion-writers";
+ALTER SCHEMA diffusion_template owner to "diffusion-writers";
+ALTER SCHEMA urdb_rates owner to "diffusion-writers";
 
 select 'ALTER TABLE ' || table_schema || '.' || table_name || ' OWNER TO "diffusion-writers";' 
 from information_schema.tables 
-where table_schema in ('diffusion_shared','diffusion_solar','diffusion_solar_config',
-			'diffusion_wind','diffusion_wind_config','geo_incentives','urdb_rates');
+where table_schema in ('diffusion_shared','diffusion_solar','diffusion_config',
+			'diffusion_wind','diffusion_template','urdb_rates');
 
 select 'ALTER TABLE ' || table_schema || '.' || table_name || ' OWNER TO "diffusion-writers";' 
 from information_schema.views
-where table_schema in ('diffusion_shared','diffusion_solar','diffusion_solar_config',
-			'diffusion_wind','diffusion_wind_config','geo_incentives','urdb_rates');
+where table_schema in ('diffusion_shared','diffusion_solar','diffusion_config',
+			'diffusion_wind','diffusion_template','urdb_rates');
 
 select 'ALTER SEQUENCE ' || sequence_schema || '.' || sequence_name || ' OWNER TO "diffusion-writers";' 
 from information_schema.sequences
-where sequence_schema in ('diffusion_shared','diffusion_solar','diffusion_solar_config',
-			'diffusion_wind','diffusion_wind_config','geo_incentives','urdb_rates');
+where sequence_schema in ('diffusion_shared','diffusion_solar','diffusion_config',
+			'diffusion_wind','diffusion_template','urdb_rates');
 
 --------------------------------------------------------------------------------
 -- functions were not copied over
 -- run:
 -- add_key_to_json.sql
--- archive
+-- clone_schema.sql
 -- get_key_from_json.sql
 -- r_array_multiply.sql
 -- r_bin_equal_interval.sql
@@ -76,7 +74,3 @@ where sequence_schema in ('diffusion_shared','diffusion_solar','diffusion_solar_
 -- wind_scoe.sql
 
 
--------------------
--- create multiple copies:
--- CREATE DATABASE diffusion_clone_2 WITH TEMPLATE diffusion_clone;
--- CREATE DATABASE diffusion_clone WITH TEMPLATE diffusion_clone_2;
