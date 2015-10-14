bebf93a839e6f7c46d38222b82538d07c7b34fae
mgleason <michael.gleason@nrel.gov>
2015-10-14 11:12:57 -0600

    adding sql script for bass params and itc options tables

---
+++ sql/data_prep/0_set_up_template_schema/2_main/4_main_itc_options.sql
@@ -0,0 +1,17 @@
+﻿SET ROLE 'diffusion-writers';
+
+DROP TABLE IF EXISTS diffusion_template.input_main_itc_options;
+CREATE TABLE diffusion_template.input_main_itc_options
+(
+  year integer NOT NULL,
+  sector text NOT NULL,
+  itc_fraction numeric NOT NULL,
+  CONSTRAINT input_main_itc_options_year_fkey FOREIGN KEY (year)
+	REFERENCES diffusion_config.sceninp_year_range (val) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE RESTRICT,
+  CONSTRAINT itc_fraction_check CHECK (itc_fraction >= 0 and itc_fraction <= 1)--,
+--   CONSTRAINT input_main_itc_options_sector_fkey foreign key (sector)
+-- 	REFERENCES diffusion_config.sceninp_sector (lower(sector)) MATCH SIMPLE
+-- 	ON UPDATE NO ACTION ON DELETE RESTRICT,	-- can't do this because input sheet is not capitalized
+);
+
---
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/21_calibrated_bass_params_solar.sql
@@ -0,0 +1,23 @@
+﻿set role 'diffusion-writers';
+DROP TABLE IF EXISTS diffusion_solar.bass_pq_calibrated_params_solar;
+CREATE TABLE diffusion_solar.bass_pq_calibrated_params_solar
+(
+  state_abbr character varying(2),
+  p numeric,
+  q numeric,
+  sector_abbr text
+);
+
+
+\COPY diffusion_solar.bass_pq_calibrated_params_solar FROM '/Volumes/Staff/mgleason/DG_Solar/Data/Source_Data/bass_parameters/bass_pq_calibrated_params_solar.csv' with csv header;
+
+select *
+FROM diffusion_solar.bass_pq_calibrated_params_solar;
+
+CREATE INDEX bass_pq_calibrated_params_solar_sector_abbr_btree
+ON diffusion_solar.bass_pq_calibrated_params_solar
+USING BTREE(sector_abbr);
+
+CREATE INDEX bass_pq_calibrated_params_solar_state_abbr_btree
+ON diffusion_solar.bass_pq_calibrated_params_solar
+USING BTREE(state_abbr);
