f8036e7f3749503ffc814c9262e61d95adc695f2
mgleason <michael.gleason@nrel.gov>
2016-02-24 11:59:31 -0700

    editing res aggregate thermal demand loading script, creating county heating fuel type loading script

--- sql/data_prep/1_load_and_prep_diffusion_shared_data/22_load_agg_thermal_demand_by_county.sql
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/22_load_agg_thermal_demand_by_county.sql
@@ -14,8 +14,12 @@ CREATE TABLE diffusion_shared.county_thermal_demand
 	sector_abbr varchar(3)
 );
 
+
+------------------------------------------------------------------------------------------------
+-- RESIDENTIAL
+
 -- load residential data
-\COPY diffusion_shared.county_thermal_demand FROM '/Volumes/Staff/mgleason/dGeo/Data/Source_Data/County_Thermal_Demand_kmccabe/simplified/residential_from_housing_units_2016_02_16.csv' with csv header;
+\COPY diffusion_shared.county_thermal_demand FROM '/Volumes/Staff/mgleason/dGeo/Data/Source_Data/County_Thermal_Demand_kmccabe/simplified/residential_from_housing_units_2016_02_24.csv' with csv header;
 
 -- fix fips codes (left pad)
 update diffusion_shared.county_thermal_demand
@@ -30,12 +34,12 @@ set fips = lpad(fips, 5, '0');
 -- check count and compare to county_geom table
 select count(*)
 FROM diffusion_shared.county_thermal_demand;
--- 3142 counties
+-- 3143 counties
 
 -- how many in county geom table?
 select count(*)
 FROM diffusion_shared.county_geom;
--- 3141 -- good sign!
+-- 3141 -- close, good sign, but still a few counties must be off
 
 -- which county_geom rows are missing from thermal demand table?
 select *
@@ -44,10 +48,10 @@ left join diffusion_shared.county_thermal_demand b
 ON lpad(a.state_fips::TEXT, 2, '0') = b.state_fips
 and a.county_fips = b.county_fips
 where b.county_fips is null;
--- Skagway-Hoonah-Angoon,Alaska
--- Bedford City,Virginia
--- Wrangell-Petersburg,Alaska
--- Prince of Wales-Outer Ketchikan,Alaska
+-- 9,Skagway-Hoonah-Angoon,Alaska,2,232
+-- 4,Wrangell-Petersburg,Alaska,2,280
+-- 3,Prince of Wales-Outer Ketchikan,Alaska,2,201
+
 
 
 -- which thermal demand rows are missing from county_geom table
@@ -65,9 +69,9 @@ where a.county_fips is null;
 -- Alaska,Wrangell City and Borough
 
 -- should be able to fix as follows
--- Hoonah-Angoon Census Area + Skagway Municipality = Skagway-Hoonah-Angoon,Alaska
--- Wrangell City and Borough + Alaska,Petersburg Borough = Wrangell-Petersburg,Alaska
--- Prince of Wales-Hyder Census Area = Prince of Wales-Outer Ketchikan,Alaska
+-- Hoonah-Angoon Census Area + Skagway Municipality = Skagway-Hoonah-Angoon,Alaska 2,232
+-- Wrangell City and Borough + Alaska,Petersburg Borough = Wrangell-Petersburg,Alaska 2,280
+-- Prince of Wales-Hyder Census Area = Prince of Wales-Outer Ketchikan,Alaska 2,201
 
 ALTER TABLE diffusion_shared.county_thermal_demand
 ADD COLUMN county_id integer;
@@ -77,23 +81,79 @@ set county_id = b.county_id
 from diffusion_shared.county_geom b
 where a.state_fips = lpad(b.state_fips::TEXT, 2, '0')
 and a.county_fips = b.county_fips;
+-- 3138 rows
 
 -- should be 5 nulls
 select *
 from diffusion_shared.county_thermal_demand
 where county_id is null;
 
---
-select *
-FROM diffusion_shared.county_thermal_demand
-where state_fips = '51'
+-- Hoonah-Angoon Census Area + Skagway Municipality = Skagway-Hoonah-Angoon,Alaska 2,232 (county_id = 9)
+INSERT INTO diffusion_shared.county_thermal_demand
+select '02' as state_fips, '232' as county_fips, '02323' as fips,
+	'Alaska' as state, 'Skagway-Hoonah-Angoon' as county,
+	sum(space_heating_thermal_load_tbtu) as space_heating_thermal_load_tbtu,
+	sum(water_heating_thermal_load_tbtu) as water_heating_thermal_load_tbtu,
+	sum(total_heating_thermal_load_tbtu) as total_heating_thermal_load_tbtu,
+	'res' as sector_abbr, 9 as county_id
+from diffusion_shared.county_thermal_demand
+where county in ('Hoonah-Angoon Census Area', 'Skagway Municipality');
 
---
-select *
-FROM diffusion_shared.county_geom
-where state_fips = 51
-and county_fips = '019'
+-- Wrangell City and Borough + Alaska,Petersburg Borough = Wrangell-Petersburg,Alaska 2,280 (county_id = 4)
+INSERT INTO diffusion_shared.county_thermal_demand
+select '02' as state_fips, '280' as county_fips, '02280' as fips,
+	'Alaska' as state, 'Wrangell-Petersburg' as county,
+	sum(space_heating_thermal_load_tbtu) as space_heating_thermal_load_tbtu,
+	sum(water_heating_thermal_load_tbtu) as water_heating_thermal_load_tbtu,
+	sum(total_heating_thermal_load_tbtu) as total_heating_thermal_load_tbtu,
+	'res' as sector_abbr, 4 as county_id
+from diffusion_shared.county_thermal_demand
+where county in ('Wrangell City and Borough', 'Petersburg Census Area');
 
+
+-- Prince of Wales-Hyder Census Area = Prince of Wales-Outer Ketchikan,Alaska 2,201 (county_id = 3)
+INSERT INTO diffusion_shared.county_thermal_demand
+select '02' as state_fips, '201' as county_fips, '02201' as fips,
+	'Alaska' as state, 'Prince of Wales-Outer Ketchikan' as county,
+	space_heating_thermal_load_tbtu,
+	water_heating_thermal_load_tbtu,
+	total_heating_thermal_load_tbtu,
+	'res' as sector_abbr, 3 as county_id
+from diffusion_shared.county_thermal_demand
+where county  = 'Prince of Wales-Hyder Census Area';
+
+-- delete the old nulls
+DELETE from diffusion_shared.county_thermal_demand
+where county_id is null;
+-- 5 rows
+
+-- cehck row count
+select count(*)
+FROM diffusion_shared.county_thermal_demand;
+-- 3141
+
+-- confirm all counties are covered now by checking alignment between datasets
 select *
-from diffusion_shared.load_and_customers_by_county_us
-where county_id = 2694
+from diffusion_shared.county_thermal_demand b
+left join diffusion_shared.county_geom a
+ON lpad(a.state_fips::TEXT, 2, '0') = b.state_fips
+and a.county_fips = b.county_fips
+where a.county_fips is null;
+-- all set
+
+-- try join on county_id
+select *
+from diffusion_shared.county_thermal_demand b
+left join diffusion_shared.county_geom a
+ON a.county_id = b.county_id
+where b.county_id is null;
+
+-- reverse direction
+select *
+from diffusion_shared.county_geom a
+left join diffusion_shared.county_thermal_demand b
+ON a.county_id = b.county_id
+where b.county_id is null;
+-- all set
+------------------------------------------------------------------------------------------------
+
