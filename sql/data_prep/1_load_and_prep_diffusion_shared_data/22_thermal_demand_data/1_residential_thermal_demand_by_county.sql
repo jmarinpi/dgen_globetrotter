0c9c81c85c33e63aceda04f92709a1670bd5cb96
mgleason <michael.gleason@nrel.gov>
2016-03-11 11:36:14 -0700

    editing res thermal demand table to include cooling

--- sql/data_prep/1_load_and_prep_diffusion_shared_data/22_thermal_demand_data/1_residential_thermal_demand_by_county.sql
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/22_thermal_demand_data/1_residential_thermal_demand_by_county.sql
@@ -8,18 +8,36 @@ CREATE TABLE diffusion_shared.county_thermal_demand
 	fips varchar(5),
 	state text,
 	county text,
+	housing_units integer,
+	county_weight numeric,
 	space_heating_thermal_load_tbtu numeric,
 	water_heating_thermal_load_tbtu numeric,
-	total_heating_thermal_load_tbtu numeric,
-	sector_abbr varchar(3)
+	space_cooling_thermal_load_tbtu numeric
 );
-
 
 ------------------------------------------------------------------------------------------------
 -- RESIDENTIAL
 
 -- load residential data
-\COPY diffusion_shared.county_thermal_demand FROM '/Volumes/Staff/mgleason/dGeo/Data/Source_Data/County_Thermal_Demand_kmccabe/simplified/residential_from_housing_units_2016_02_24.csv' with csv header;
+\COPY diffusion_shared.county_thermal_demand FROM '/Volumes/Staff/mgleason/dGeo/Data/Source_Data/Thermal_Demand_kmccabe/simplified/residential_from_housing_units_2016_02_29.csv' with csv header;
+
+-- add total_heating_thermal_load_tbtu numeric,
+ALTER TABLE diffusion_shared.county_thermal_demand
+ADD COLUMN total_heating_thermal_load_tbtu numeric;
+
+UPDATE diffusion_shared.county_thermal_demand
+set total_heating_thermal_load_tbtu = space_heating_thermal_load_tbtu + water_heating_thermal_load_tbtu;
+-- 3143 rows
+
+
+-- add sector_abbr column
+ALTER TABLE diffusion_shared.county_thermal_demand
+ADD COLUMN sector_abbr varchar(3);
+
+UPDATE diffusion_shared.county_thermal_demand
+set sector_abbr = 'res';
+-- 3143 rows
+
 
 -- fix fips codes (left pad)
 update diffusion_shared.county_thermal_demand
@@ -51,8 +69,6 @@ where b.county_fips is null;
 -- 9,Skagway-Hoonah-Angoon,Alaska,2,232
 -- 4,Wrangell-Petersburg,Alaska,2,280
 -- 3,Prince of Wales-Outer Ketchikan,Alaska,2,201
-
-
 
 -- which thermal demand rows are missing from county_geom table
 -- which county_geom rows are missing from thermal demand table?
@@ -89,11 +105,13 @@ from diffusion_shared.county_thermal_demand
 where county_id is null;
 
 -- Hoonah-Angoon Census Area + Skagway Municipality = Skagway-Hoonah-Angoon,Alaska 2,232 (county_id = 9)
-INSERT INTO diffusion_shared.county_thermal_demand
+INSERT INTO diffusion_shared.county_thermal_demand 
 select '02' as state_fips, '232' as county_fips, '02323' as fips,
 	'Alaska' as state, 'Skagway-Hoonah-Angoon' as county,
+	sum(housing_units) as housing_units, sum(county_weight) as county_weight,
 	sum(space_heating_thermal_load_tbtu) as space_heating_thermal_load_tbtu,
 	sum(water_heating_thermal_load_tbtu) as water_heating_thermal_load_tbtu,
+	sum(space_cooling_thermal_load_tbtu) as space_cooling_thermal_load_tbtu,
 	sum(total_heating_thermal_load_tbtu) as total_heating_thermal_load_tbtu,
 	'res' as sector_abbr, 9 as county_id
 from diffusion_shared.county_thermal_demand
@@ -103,8 +121,10 @@ where county in ('Hoonah-Angoon Census Area', 'Skagway Municipality');
 INSERT INTO diffusion_shared.county_thermal_demand
 select '02' as state_fips, '280' as county_fips, '02280' as fips,
 	'Alaska' as state, 'Wrangell-Petersburg' as county,
+	sum(housing_units) as housing_units, sum(county_weight) as county_weight,
 	sum(space_heating_thermal_load_tbtu) as space_heating_thermal_load_tbtu,
 	sum(water_heating_thermal_load_tbtu) as water_heating_thermal_load_tbtu,
+	sum(space_cooling_thermal_load_tbtu) as space_cooling_thermal_load_tbtu,
 	sum(total_heating_thermal_load_tbtu) as total_heating_thermal_load_tbtu,
 	'res' as sector_abbr, 4 as county_id
 from diffusion_shared.county_thermal_demand
@@ -115,9 +135,11 @@ where county in ('Wrangell City and Borough', 'Petersburg Census Area');
 INSERT INTO diffusion_shared.county_thermal_demand
 select '02' as state_fips, '201' as county_fips, '02201' as fips,
 	'Alaska' as state, 'Prince of Wales-Outer Ketchikan' as county,
-	space_heating_thermal_load_tbtu,
-	water_heating_thermal_load_tbtu,
-	total_heating_thermal_load_tbtu,
+	sum(housing_units) as housing_units, sum(county_weight) as county_weight,
+	sum(space_heating_thermal_load_tbtu) as space_heating_thermal_load_tbtu,
+	sum(water_heating_thermal_load_tbtu) as water_heating_thermal_load_tbtu,
+	sum(space_cooling_thermal_load_tbtu) as space_cooling_thermal_load_tbtu,
+	sum(total_heating_thermal_load_tbtu) as total_heating_thermal_load_tbtu,
 	'res' as sector_abbr, 3 as county_id
 from diffusion_shared.county_thermal_demand
 where county  = 'Prince of Wales-Hyder Census Area';
@@ -155,5 +177,37 @@ left join diffusion_shared.county_thermal_demand b
 ON a.county_id = b.county_id
 where b.county_id is null;
 -- all set
+
+-- make sure no nulls in the numeric values
+select count(*)
+FROM diffusion_shared.county_thermal_demand
+where space_heating_thermal_load_tbtu IS NULL OR
+	water_heating_thermal_load_tbtu IS NULL OR
+	space_cooling_thermal_load_tbtu IS NULL OR
+	total_heating_thermal_load_tbtu IS NULL ;
+-- 0, all set!
+
+-- check for cases where water heating is more than space heating
+select *
+FROM diffusion_shared.county_thermal_demand
+where space_heating_thermal_load_tbtu < water_heating_thermal_load_tbtu;
+-- 82 rows
+-- all in FL and AZ -- makes sense
+
+-- how about cooling exceeding space heating?
+select *
+FROM diffusion_shared.county_thermal_demand
+where space_heating_thermal_load_tbtu < space_cooling_thermal_load_tbtu;
+-- all in FL and AZ -- makes sense
+-- same 82 rows 
+
+-- how many total rows in AZ and FL?
+select count(*)
+FROM diffusion_shared.county_thermal_demand
+where state in ('Arizona', 'Florida');
+-- 82 rows
+-- this makes sense -- the aggregate state totals must have shown 
+-- cooling > space heating and water heating > space heating
+-- and this was applied universally to all counties in the states
 ------------------------------------------------------------------------------------------------
 
