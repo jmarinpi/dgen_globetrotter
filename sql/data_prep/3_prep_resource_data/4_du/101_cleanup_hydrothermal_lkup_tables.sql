f5334555cee7cd2ab0eed049a4525c644d9f78fc
mgleason <michael.gleason@nrel.gov>
2016-07-08 17:45:54 -0600

    starting to work on hydrothermal cleanup script

---
+++ sql/data_prep/3_prep_resource_data/4_du/101_cleanup_hydrothermal_lkup_tables.sql
@@ -0,0 +1,74 @@
+﻿-- change the table owner
+set role 'server-superusers';
+ALTER TABLE diffusion_geo.hydro_poly_tracts
+  OWNER TO "diffusion-writers";
+ALTER TABLE diffusion_geo.hydro_pt_tracts
+  OWNER TO "diffusion-writers";
+set role 'diffusion-writers';
+
+
+
+-- create indices
+CREATE INDEX hydro_poly_tracts_btree_tract_id_alias
+ON diffusion_geo.hydro_poly_tracts
+USING BTREE(tract_id_alias);
+
+CREATE INDEX hydro_pt_tracts_btree_tract_id_alias
+ON diffusion_geo.hydro_pt_tracts
+USING BTREE(tract_id_alias);
+
+CREATE INDEX hydro_poly_tracts_btree_resource_uid
+ON diffusion_geo.hydro_poly_tracts
+USING BTREE(resource_uid);
+
+CREATE INDEX hydro_pt_tracts_btree_resource_uid
+ON diffusion_geo.hydro_pt_tracts
+USING BTREE(resource_uid);
+
+-- add primary key
+ALTER TABLE diffusion_geo.hydro_pt_tracts
+ADD PRIMARY KEY (tract_id_alias, resource_uid);
+-- rejected beacuse tract_id_alias has nulls
+
+ALTER TABLE diffusion_geo.hydro_poly_tracts
+ADD PRIMARY KEY (tract_id_alias, resource_uid);
+-- rejected because of duplicate combination
+
+-- fix these issues
+-- POINTS
+select *
+FROM diffusion_geo.hydro_pt_tracts
+where tract_id_alias is null;
+-- two in CA, two in AK -- assume CA are offshore, so just delete
+
+DELETE
+FROM diffusion_geo.hydro_pt_tracts
+where tract_id_alias is null;
+-- 4 rows deleted
+
+-- add the primary key
+
+-- why are they null?
+select cell_gid
+FROM diffusion_geo.egs_lkup
+where tract_id_alias is null
+group by cell_gid;
+-- reviewed in Q and they are all coastal, which is fine
+
+-- delte them
+DELETE 
+FROM diffusion_geo.egs_lkup
+where tract_id_alias is null;
+-- 100 deleted
+
+-- change the table name
+ALTER TABLE diffusion_geo.egs_lkup
+RENAME TO egs_tract_id_alias_lkup;
+
+-- add the primary key
+ALTER TABLE diffusion_geo.egs_tract_id_alias_lkup
+ADD PRIMARY KEY (tract_id_alias, cell_gid);
+
+-- change the cell gid to integfer
+ALTER TABLE diffusion_geo.egs_tract_id_alias_lkup
+ALTER COLUMN cell_gid type integer using cell_gid::INTEGER;
