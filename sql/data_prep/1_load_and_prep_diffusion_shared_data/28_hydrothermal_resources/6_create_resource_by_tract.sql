48ba33d80f07be995f611befbd308c38f4b4a176
Meghan Mooney <meghan.mooney@nrel.gov>
2016-07-12 11:49:03 -0600

    updated lkup sql and resource breakdown by tract sql

--- sql/data_prep/1_load_and_prep_diffusion_shared_data/28_hydrothermal_resources/6_create_resource_by_tract.sql
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/28_hydrothermal_resources/6_create_resource_by_tract.sql
@@ -15,13 +15,24 @@ create table diffusion_geo.hydro_poly_tracts as
 		a.area_of_intersection_sqkm as area_of_res_in_tract_sqkm,
 		b.res_thickness_km,
 		b.area_per_well_km2 as area_per_well_sqkm,
-		(a.area_of_intersection_sqkm/b.area_per_well_km2) as n_wells_in_tract,
-		diffusion_geo.extractable_resource_joules_production_plan((a.area_of_intersection_sqkm/b.area_per_well_km2), b.res_temp_deg_c)/3600000000 as extractable_resource_in_tract_mwh,
-		b.mean_resource_1e18_joules/3600000000 as extractable_resource_per_well_in_tract_mwh
+		round((a.area_of_intersection_sqkm/b.area_per_well_km2),0) as n_wells_in_tract,
+		round(diffusion_geo.extractable_resource_joules_production_plan((a.area_of_intersection_sqkm/b.area_per_well_km2), b.res_temp_deg_c)/3600000000, 3) as extractable_resource_in_tract_mwh,
+		NULL::numeric as extractable_resource_per_well_in_tract_mwh
 		from diffusion_geo.hydro_poly_lkup a
 		left join diffusion_geo.resources_hydrothermal_poly b
 		on a.resource_uid = b.uid
 	);
+	-- 3127 rows affected = total # from lkup √
+
+-- Delete any records where n_wells_in_tract = 0
+delete from diffusion_geo.hydro_poly_tracts
+where n_wells_in_tract = 0;
+
+-- Update resource_per_well_in_tract
+update diffusion_geo.hydro_poly_tracts
+	set extractable_resource_per_well_in_tract_mwh = round(extractable_resource_in_tract_mwh / n_wells_in_tract, 3);
+
+-- Total = 1,388 (after removing most records of 0 n_wells_in_tract)
 
 -- ******************-- ******************-- ******************
 -- * NOte: I calcualted reservoir thickenss by km (not m) to be consistent with the xlsx
