01e61620ee46ca210d9e9ab8408c483c359f3bd9
mgleason <michael.gleason@nrel.gov>
2016-07-12 10:46:11 -0600

    closing #608

--- sql/data_prep/4_load_misc_technology_specific_tables/3_ghp/1_load_eia_microdata_to_baseline_system_lkup.sql
+++ sql/data_prep/4_load_misc_technology_specific_tables/3_ghp/1_load_eia_microdata_to_baseline_system_lkup.sql
@@ -1,4 +1,4 @@
-SET ROLE 'diffusion-writers';
+﻿SET ROLE 'diffusion-writers';
 DROP TABLE IF EXISTS diffusion_geo.eia_microdata_to_baseline_system_types_lkup;
 CREATE TABLE diffusion_geo.eia_microdata_to_baseline_system_types_lkup
 (
@@ -8,16 +8,20 @@ CREATE TABLE diffusion_geo.eia_microdata_to_baseline_system_types_lkup
 	space_cool_equip text,
 	space_cool_fuel text,
 	baseline_type text,
-	test numeric
+	combo_ct numeric
 );
 
-\COPY diffusion_geo.eia_microdata_to_baseline_system_types_lkup FROM '/Users/mmooney/Desktop/spht_cool_scenario_mapping[2].csv' with csv header;
+\COPY diffusion_geo.eia_microdata_to_baseline_system_types_lkup FROM '/Volumes/Staff/mgleason/dGeo/Data/Source_Data/eia_microdata_to_baseline_systems_from_kmccabe/spht_cool_scenario_mapping_11-7-2016.csv' with csv header;
 
 
 
 update diffusion_geo.eia_microdata_to_baseline_system_types_lkup
 set baseline_type = NULL where baseline_type = 'NA';
+-- 60 rows affected
 
 alter table diffusion_geo.eia_microdata_to_baseline_system_types_lkup
-drop column test,
+drop column combo_ct,
 alter column baseline_type type integer using (baseline_type::integer);
+
+select *
+FROM diffusion_geo.eia_microdata_to_baseline_system_types_lkup;
