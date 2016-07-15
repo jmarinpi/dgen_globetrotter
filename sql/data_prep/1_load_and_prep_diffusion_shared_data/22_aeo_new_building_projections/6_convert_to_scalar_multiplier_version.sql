6b63f6a1fe5cd46a2f5a7e6d5d0450c79ad1885f
mgleason <michael.gleason@nrel.gov>
2016-07-15 08:39:04 -0600

    creating dummy data for building growth multipliers, revising scenario names and input setup for buildin growht -- per #574

---
+++ sql/data_prep/1_load_and_prep_diffusion_shared_data/22_aeo_new_building_projections/6_convert_to_scalar_multiplier_version.sql
@@ -0,0 +1,50 @@
+﻿set role 'diffusion-writers';
+
+DROP TABLE if exists diffusion_shared.aeo_new_building_multipliers_2015;
+CREATE TABLE diffusion_shared.aeo_new_building_multipliers_2015 AS
+select state_fips, state, census_division, year, scenario, 
+	1.02::NUMERIC as housing_starts_single_family_millions, 
+        1.02::NUMERIC AS housing_starts_multi_family_millions, 
+        1.02::NUMERIC AS commercial_sq_ft_billions, 
+       state_abbr, census_division_abbr
+from  diffusion_shared.aeo_new_building_projections_2015;
+
+-- add primary key on state, year, scenario
+ALTER TABLE diffusion_shared.aeo_new_building_multipliers_2015
+ADD PRIMARY KEY (state_abbr, year, scenario);
+
+-- add indices
+create INDEX aeo_new_building_multipliers_2015_btree_state_abbr
+on diffusion_shared.aeo_new_building_multipliers_2015
+using btree(state_abbr);
+
+create INDEX aeo_new_building_multipliers_2015_btree_year
+on diffusion_shared.aeo_new_building_multipliers_2015
+using btree(year);
+
+create INDEX aeo_new_building_multipliers_2015_btree_scenario
+on diffusion_shared.aeo_new_building_multipliers_2015
+using btree(scenario);
+
+-- change the names for the scenarios to our standard naming conventions
+select distinct scenario
+from diffusion_shared.aeo_new_building_multipliers_2015;
+-- Reference --> AEO2015 Reference
+-- High Growth --> AEO2015  High Growth
+-- Low Growth --> AEO2015 Low Growth
+-- Low Price --> AEO2015 Low Prices
+-- High Price --> AEO2015 High Prices
+
+UPDATE diffusion_shared.aeo_new_building_multipliers_2015
+set scenario = 
+	CASE
+		WHEN scenario = 'Reference' THEN 'AEO2015 Reference'
+		WHEN scenario = 'High Growth' THEN 'AEO2015 High Growth'
+		WHEN scenario = 'Low Growth' THEN 'AEO2015 Low Growth'
+		WHEN scenario = 'Low Price' THEN 'AEO2015 Low Prices'
+		WHEN scenario = 'High Price' THEN 'AEO2015 High Prices'
+	end;
+
+-- check results
+select distinct scenario
+from diffusion_shared.aeo_new_building_multipliers_2015;
--- sql/data_prep/2_set_up_template_schema/2_main/1_main_scenario_options.sql
+++ sql/data_prep/2_set_up_template_schema/2_main/1_main_scenario_options.sql
@@ -23,8 +23,7 @@ CREATE TABLE diffusion_template.input_main_scenario_options
 	com_max_market_curve text NOT NULL,
 	ind_max_market_curve text NOT NULL,
 	carbon_price text NOT NULL,
-	com_building_growth_scenario text not null,
-	res_building_growth_scenario text not null,
+	new_building_growth_scenario text not null,
 	regional_heating_fuel_cost_trajectories text not null,
 	random_generator_seed integer NOT NULL, -- doesn't need a constraint -- just needs to be integer
 	-- add check/fkey constraints to ensure only valid values are entered in each column
