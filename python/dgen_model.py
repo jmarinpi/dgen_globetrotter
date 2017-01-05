fixed broadcasting issue in csv writer

python/dgen_model.py
@@ -750,7 +750,7 @@ def main(mode = None, resume_year = None, endyear = None, ReEDS_inputs = None):
                     
 #                    # I am assuming this is a competition metric.
                     # TODO: dig in and delete this, since it isn't being used anymore
-                    df['selected_option'] = True                    
+#                    df['selected_option'] = True                    
                     
                     # calculate diffusion based on economics and bass diffusion                   
                     df, market_last_year_df = diffusion_functions_elec.calc_diffusion_storage(df, is_first_year, bass_params) 
@@ -809,8 +809,9 @@ def main(mode = None, resume_year = None, endyear = None, ReEDS_inputs = None):
                     
                     solar_cf_all_adopters_year = generation_all_adopters_year
                     solar_cf_all_adopters_year = pd.merge(generation_all_adopters_year, pca_reg_cum_capacities, on='pca_reg')
-                    solar_cf_all_adopters_year[hour_list] = solar_cf_all_adopters_year[hour_list] / np.array(solar_cf_all_adopters_year['pv_kw_cum'])                    
-
+                    solar_cf_all_adopters_year[hour_list] = np.array(solar_cf_all_adopters_year[hour_list]) / np.array(solar_cf_all_adopters_year['pv_kw_cum']).reshape(len(solar_cf_all_adopters_year),1)        
+                    solar_cf_all_adopters_year = solar_cf_all_adopters_year.fillna(0)
+                    
                     solar_cf_all_adopters = solar_cf_all_adopters.append(solar_cf_all_adopters_year)
                     solar_cf_all_adopters = solar_cf_all_adopters[['pca_reg', 'year'] + hour_list]
                     solar_cf_all_adopters.to_csv(out_scen_path + '/pv_cf_by_pca_and_year.csv', index=False)                     

python/post_processor_storage.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 
 
 
-agent_df_dir = 'agent_df_pickles_ca_with_deg'
+agent_df_dir = 'agent_df_pickles_national_lowcost'
 
 years = np.arange(2014,2051,2)
 
@@ -56,27 +56,27 @@ plt.ylabel('GWdc')
 plt.legend(['PV', 'Storage'])
 plt.axis([2015, 2050, 0, np.max(results_df['pv_kw_cum']/1e6*1.2)])
 
-##%%
-#plt.figure(1)
-#ratio = results_df['batt_kw_ann'] / results_df['pv_kw_ann']
-#plt.plot(results_df.index+1, ratio)
-#plt.grid(True)
-##%%
-#plt.figure(2)
-#plt.plot(results_df.index+1, results_df['pv_kw_cum']/1000)
-#plt.plot(results_df.index+1, results_df['pv_kw_cum_res']/1000)
-#plt.plot(results_df.index+1, results_df['pv_kw_cum_com']/1000)
-#plt.plot(results_df.index+1, results_df['pv_kw_cum_ind']/1000)
-#plt.legend(['total', 'res', 'com', 'ind'])
-#plt.grid(True)
-#
-##%%
-#plt.figure(3)
-#plt.plot(results_df.index+1, results_df['batt_kwh_cum']/1000)
-#plt.plot(results_df.index+1, results_df['batt_kwh_cum_res']/1000)
-#plt.plot(results_df.index+1, results_df['batt_kwh_cum_com']/1000)
-#plt.plot(results_df.index+1, results_df['batt_kwh_cum_ind']/1000)
-#plt.legend(['total', 'res', 'com', 'ind'])
-#plt.grid(True)
+#%%
+plt.figure(1)
+ratio = results_df['batt_kw_ann'] / results_df['pv_kw_ann']
+plt.plot(results_df.index+1, ratio)
+plt.grid(True)
+#%%
+plt.figure(2)
+plt.plot(results_df.index+1, results_df['pv_kw_cum']/1000)
+plt.plot(results_df.index+1, results_df['pv_kw_cum_res']/1000)
+plt.plot(results_df.index+1, results_df['pv_kw_cum_com']/1000)
+plt.plot(results_df.index+1, results_df['pv_kw_cum_ind']/1000)
+plt.legend(['total', 'res', 'com', 'ind'])
+plt.grid(True)
+
+#%%
+plt.figure(3)
+plt.plot(results_df.index+1, results_df['batt_kwh_cum']/1000)
+plt.plot(results_df.index+1, results_df['batt_kwh_cum_res']/1000)
+plt.plot(results_df.index+1, results_df['batt_kwh_cum_com']/1000)
+plt.plot(results_df.index+1, results_df['batt_kwh_cum_ind']/1000)
+plt.legend(['total', 'res', 'com', 'ind'])
+plt.grid(True)
 
 