f2a62c16b8cf62993ff5e8e4a8f99adda2fbe600
pieter.gagnon@nrel.gov <pieter.gagnon@nrel.gov>
2017-04-09 17:11:26 -0600

    cosmetic changes to storage functions

--- python/storage_functions.py
+++ python/storage_functions.py
@@ -59,8 +59,7 @@ def calc_system_size_and_financial_performance(agent):
     batt = dFuncs.Battery()
     batt_ratio = 3.0
 
-    tariff_dict = agent['tariff_dict']
-    tariff = tFuncs.Tariff(dict_obj=tariff_dict)
+    tariff = tFuncs.Tariff(dict_obj=agent['tariff_dict'])
 
     # Create export tariff object
     if agent['nem_system_size_limit_kw'] != 0: 
@@ -79,8 +78,6 @@ def calc_system_size_and_financial_performance(agent):
     # Estimate bill savings revenue from a set of solar+storage system sizes
     #=========================================================================#    
     # Set PV sizes to evaluate
-#    pv_inc = 1
-#    pv_sizes = np.linspace(0, agent['max_pv_size']*0.95, pv_inc)
     if export_tariff.full_retail_nem==True:
         pv_sizes = np.array([agent['max_pv_size']*0.95])
     else:
