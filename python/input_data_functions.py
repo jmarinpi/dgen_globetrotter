0e566498eb55a5005aa5906660400b0ed1f1cd23
pdas <paritosh.das@nrel.gov>
2017-06-12 15:03:17 -0600

    making the column names in input csv for pv_prices same as is expected in code and getting rid of redundant code

--- python/input_data_functions.py
+++ python/input_data_functions.py
@@ -60,7 +60,7 @@ def import_table(scenario_settings, con, input_name, csv_import_function):
         else:
             
             df = pd.read_csv(os.path.join(input_data_dir, input_name, scenario_userdefined_value), index_col=None)
-            df = csv_import_function(df, scenario_settings)
+            df = csv_import_function(df)
 
             # To Do: Ted working on fix to import dataframe into postgres database table
             #df.to_sql(scenario_userdefined_value, con, schema=schema)
@@ -72,7 +72,7 @@ def import_table(scenario_settings, con, input_name, csv_import_function):
     return df
 
 #%%
-def stacked_sectors(df, scenario_settings):
+def stacked_sectors(df):
     sectors = ['res', 'ind','com','nonres']
     output = pd.DataFrame()
     core_columns = [x for x in df.columns if x.split("_")[-1] not in sectors]
@@ -92,7 +92,7 @@ def stacked_sectors(df, scenario_settings):
     return output
 
 #%%
-def deprec_schedule(df, scenario_settings):
+def deprec_schedule(df):
 
     columns = ['1', '2', '3', '4', '5', '6']
     df['deprec_sch']=df.apply(lambda x: [x.to_dict()[y] for y in columns], axis=1)
@@ -106,12 +106,12 @@ def deprec_schedule(df, scenario_settings):
         last_entry['year'] = year
         df = df.append(last_entry)
 
-    return df.ix[:,['year','deprec_sch']]
+    return df.ix[:,['year','sector_abbr','deprec_sch']]
 
 #%%
 def melt_year(paramater_name):
 
-    def function(df,scenario_settings):
+    def function(df):
 
         years = np.arange(2014, 2051, 2)
         years = [str(year) for year in years]
@@ -123,186 +123,3 @@ def melt_year(paramater_name):
         return df_tify
 
     return function
-
-
-#%%
-def process_pv_price_trajectories(pv_price_traj, scenario_settings):
-
-    pv_price_traj.to_csv(scenario_settings.dir_to_write_input_data + '/pv_prices.csv', index=False)
-    
-    res_df = pd.DataFrame(pv_price_traj['year'])
-    res_df = pv_price_traj[['year', 'pv_price_res', 'pv_om_res', 'pv_variable_om_res']]
-    res_df.rename(columns={'pv_price_res':'pv_price_per_kw', 
-                           'pv_om_res':'pv_om_per_kw',
-                           'pv_variable_om_res':'pv_variable_om_per_kw'}, inplace=True)
-    res_df['sector_abbr'] = 'res'
-    
-    com_df = pd.DataFrame(pv_price_traj['year'])
-    com_df = pv_price_traj[['year', 'pv_price_com', 'pv_om_com', 'pv_variable_om_com']]
-    com_df.rename(columns={'pv_price_com':'pv_price_per_kw', 
-                           'pv_om_com':'pv_om_per_kw',
-                           'pv_variable_om_com':'pv_variable_om_per_kw'}, inplace=True)
-    com_df['sector_abbr'] = 'com'
-    
-    ind_df = pd.DataFrame(pv_price_traj['year'])
-    ind_df = pv_price_traj[['year', 'pv_price_ind', 'pv_om_ind', 'pv_variable_om_ind']]
-    ind_df.rename(columns={'pv_price_ind':'pv_price_per_kw', 
-                           'pv_om_ind':'pv_om_per_kw',
-                           'pv_variable_om_ind':'pv_variable_om_per_kw'}, inplace=True)
-    ind_df['sector_abbr'] = 'ind'
-    
-    pv_price_traj = pd.concat([res_df, com_df, ind_df], ignore_index=True)
-    
-    return pv_price_traj
-
-
-#%%
-def process_pv_tech_performance(pv_tech_traj_df, scenario_settings):
-    
-    pv_tech_traj_df.to_csv(scenario_settings.dir_to_write_input_data + '/pv_tech_performance.csv', index=False)
-    
-    res_df = pd.DataFrame(pv_tech_traj_df['year'])
-    res_df = pv_tech_traj_df[['year', 'pv_deg_res', 'pv_power_density_w_per_sqft_res']]
-    res_df.rename(columns={'pv_deg_res':'pv_deg',
-                           'pv_power_density_w_per_sqft_res':'pv_power_density_w_per_sqft'}, inplace=True)
-    res_df['sector_abbr'] = 'res'
-    
-    com_df = pd.DataFrame(pv_tech_traj_df['year'])
-    com_df = pv_tech_traj_df[['year', 'pv_deg_com', 'pv_power_density_w_per_sqft_com']]
-    com_df.rename(columns={'pv_deg_com':'pv_deg',
-                           'pv_power_density_w_per_sqft_com':'pv_power_density_w_per_sqft'}, inplace=True)
-    com_df['sector_abbr'] = 'com'
-    
-    ind_df = pd.DataFrame(pv_tech_traj_df['year'])
-    ind_df = pv_tech_traj_df[['year', 'pv_deg_ind', 'pv_power_density_w_per_sqft_ind']]
-    ind_df.rename(columns={'pv_deg_ind':'pv_deg',
-                           'pv_power_density_w_per_sqft_ind':'pv_power_density_w_per_sqft'}, inplace=True)
-    ind_df['sector_abbr'] = 'ind'
-    
-    pv_tech_traj_df = pd.concat([res_df, com_df, ind_df], ignore_index=True)
-
-    return pv_tech_traj_df
-
-
-    #%%
-def process_batt_price_trajectories(batt_price_traj, scenario_settings):
-    
-    batt_price_traj.to_csv(scenario_settings.dir_to_write_input_data + '/batt_prices.csv', index=False)
-
-    res_df = pd.DataFrame(batt_price_traj['year'])
-    res_df = batt_price_traj[['year', 'batt_price_per_kwh_res', 'batt_price_per_kw_res',
-                              'batt_om_per_kw_res', 'batt_om_per_kwh_res', 'batt_replace_frac_kw', 'batt_replace_frac_kwh']]
-    res_df.rename(columns={'batt_price_per_kwh_res':'batt_price_per_kwh', 
-                           'batt_price_per_kw_res':'batt_price_per_kw',
-                           'batt_om_per_kw_res':'batt_om_per_kw',
-                           'batt_om_per_kwh_res':'batt_om_per_kwh'}, inplace=True)
-    res_df['sector_abbr'] = 'res'
-    
-    com_df = pd.DataFrame(batt_price_traj['year'])
-    com_df = batt_price_traj[['year', 'batt_price_per_kwh_nonres', 'batt_price_per_kw_nonres',
-                              'batt_om_per_kw_nonres', 'batt_om_per_kwh_nonres', 'batt_replace_frac_kw', 'batt_replace_frac_kwh']]
-    com_df.rename(columns={'batt_price_per_kwh_nonres':'batt_price_per_kwh', 
-                           'batt_price_per_kw_nonres':'batt_price_per_kw',
-                           'batt_om_per_kw_nonres':'batt_om_per_kw',
-                           'batt_om_per_kwh_nonres':'batt_om_per_kwh'}, inplace=True)
-    com_df['sector_abbr'] = 'com'
-    
-    ind_df = pd.DataFrame(batt_price_traj['year'])
-    ind_df = batt_price_traj[['year', 'batt_price_per_kwh_nonres', 'batt_price_per_kw_nonres',
-                              'batt_om_per_kw_nonres', 'batt_om_per_kwh_nonres', 'batt_replace_frac_kw', 'batt_replace_frac_kwh']]
-    ind_df.rename(columns={'batt_price_per_kwh_nonres':'batt_price_per_kwh', 
-                           'batt_price_per_kw_nonres':'batt_price_per_kw',
-                           'batt_om_per_kw_nonres':'batt_om_per_kw',
-                           'batt_om_per_kwh_nonres':'batt_om_per_kwh'}, inplace=True)
-    ind_df['sector_abbr'] = 'ind'
-    
-    batt_price_traj = pd.concat([res_df, com_df, ind_df], ignore_index=True)
-    
-    return batt_price_traj
-
-
-    #%%
-def process_financing_terms(financing_terms, scenario_settings):
-    
-    financing_terms.to_csv(scenario_settings.dir_to_write_input_data + '/financing_terms.csv', index=False)
-    
-    res_df = pd.DataFrame(financing_terms['year'])
-    res_df = financing_terms[['year', 'economic_lifetime', 'loan_term_res', 'loan_rate_res', 'down_payment_res', 'real_discount_res', 'tax_rate_res']]
-    res_df.rename(columns={'loan_term_res':'loan_term', 
-                           'loan_rate_res':'loan_rate', 
-                           'down_payment_res':'down_payment', 
-                           'real_discount_res':'real_discount', 
-                           'tax_rate_res':'tax_rate'}, inplace=True)
-    res_df['sector_abbr'] = 'res'
-    
-    com_df = pd.DataFrame(financing_terms['year'])
-    com_df = financing_terms[['year', 'economic_lifetime', 'loan_term_nonres', 'loan_rate_nonres', 'down_payment_nonres', 'real_discount_nonres', 'tax_rate_nonres']]
-    com_df.rename(columns={'loan_term_nonres':'loan_term', 
-                           'loan_rate_nonres':'loan_rate', 
-                           'down_payment_nonres':'down_payment', 
-                           'real_discount_nonres':'real_discount', 
-                           'tax_rate_nonres':'tax_rate'}, inplace=True)
-    com_df['sector_abbr'] = 'com'
-    
-    ind_df = pd.DataFrame(financing_terms['year'])
-    ind_df = financing_terms[['year', 'economic_lifetime', 'loan_term_nonres', 'loan_rate_nonres', 'down_payment_nonres', 'real_discount_nonres', 'tax_rate_nonres']]
-    ind_df.rename(columns={'loan_term_nonres':'loan_term', 
-                           'loan_rate_nonres':'loan_rate', 
-                           'down_payment_nonres':'down_payment', 
-                           'real_discount_nonres':'real_discount', 
-                           'tax_rate_nonres':'tax_rate'}, inplace=True)
-    ind_df['sector_abbr'] = 'ind'
-    
-    financing_terms = pd.concat([res_df, com_df, ind_df], ignore_index=True)
-
-    return financing_terms
-
-
-#%%
-def process_batt_tech_performance(batt_tech_traj, scenario_settings):
-    
-    batt_tech_traj.to_csv(scenario_settings.dir_to_write_input_data + '/batt_tech_performance.csv', index=False)
-    
-    res_df = pd.DataFrame(batt_tech_traj['year'])
-    res_df = batt_tech_traj[['year', 'batt_eff_res', 'batt_lifetime_res']]
-    res_df.rename(columns={'batt_eff_res':'batt_eff', 
-                           'batt_lifetime_res':'batt_lifetime'}, inplace=True)
-    res_df['sector_abbr'] = 'res'
-    
-    com_df = pd.DataFrame(batt_tech_traj['year'])
-    com_df = batt_tech_traj[['year', 'batt_eff_com', 'batt_lifetime_com']]
-    com_df.rename(columns={'batt_eff_com':'batt_eff', 
-                           'batt_lifetime_com':'batt_lifetime'}, inplace=True)
-    com_df['sector_abbr'] = 'com'
-    
-    ind_df = pd.DataFrame(batt_tech_traj['year'])
-    ind_df = batt_tech_traj[['year', 'batt_eff_ind', 'batt_lifetime_ind']]
-    ind_df.rename(columns={'batt_eff_ind':'batt_eff', 
-                           'batt_lifetime_ind':'batt_lifetime'}, inplace=True)
-    ind_df['sector_abbr'] = 'ind'
-    
-    batt_tech_traj = pd.concat([res_df, com_df, ind_df], ignore_index=True)
-    
-    return batt_tech_traj
-
-
-#%%
-def process_depreciation_schedules(deprec_schedules, scenario_settings):
-    
-    deprec_schedules.to_csv(scenario_settings.dir_to_write_input_data + '/depreciation_schedules.csv', index=False)
-    
-    deprec_schedules['deprec_sch'] = 'temp'
-    
-    for index in deprec_schedules.index:
-        deprec_schedules.set_value(index, 'deprec_sch', np.array(deprec_schedules.loc[index, ['1','2','3','4','5','6']]))
-
-    max_required_year = 2050
-    max_input_year = np.max(deprec_schedules['year'])
-    missing_years = np.arange(max_input_year+1, max_required_year+1, 1)
-    last_entry = deprec_schedules[deprec_schedules['year']==max_input_year]
-    
-    for year in missing_years:
-        last_entry['year'] = year
-        deprec_schedules = deprec_schedules.append(last_entry)
-        
-    return deprec_schedules[['year', 'sector_abbr', 'deprec_sch']]
