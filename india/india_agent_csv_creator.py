186bd80ff4ba031c394b1356422395acf6b37927
Sam Koebrich <sam.koebrich@NREL.gov>
2020-03-13 13:57:36 -0600

    starting india agent creation file.

--- india/india_agent_csv_creator.py
+++ india/india_agent_csv_creator.py
@@ -13,13 +13,12 @@ TODO:
     - move agent creation into dgen_model based on params in config? 
 """
 
+import pandas as pd
 
 #%%
 """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 """~~~~~~~~~~~~~~~~~ Set-up ~~~~~~~~~~~~~~~~"""
 """~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
-
-EXCHANGE_RATE = 70 #rupee in 1 USD
 
 # --- Define Helper Functions ---
 
@@ -54,6 +53,8 @@ Columns
     developable_buildings_pct (float) : pct of customers represented by agent that can install solar
     bldg_size_class	(str) : 'small' how is this used? TODO
     developable_roof_sqft (int) : total installable rooftop area TODO move this to a seperate csv? 
+    social_indicator (float) : a linear scaler indicating social development within a given region
+        for instance, see 'hdi' for Mexico
 """
 
 """
@@ -88,19 +89,12 @@ Columns
     2014-2050 (float) : TODO how is this used? 
 """
 
-"""
---- discount_rates.csv ---
-
-Columns
--------
-    state_id (int) : integer representation of state
-    sector_abbr (str) : the sector of the agent
-    real_discount (float) : the discount rate of the state/sector in percent TODO: how is this different than financing_rates.csv?
-"""
+#%%
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~ Financing Rates ~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
 """
---- financing_rates.csv ---
-
 Columns
 -------
     state_id (int) : integer representation of state
@@ -109,10 +103,46 @@ Columns
     real_discount (float) : the discount rate of the state/sector in percent
     down_payment (float) : percent of downpayment towards system (typically 0.2 to compare apples to apples in WACC)
 """
+# --- Take dict of controlregions by social indicator ---
+state_social_df = agent_df[['state_id','social_indicator']].drop_duplicates()
+
+# --- Permute by sector ---
+social_dfs = []
+for s in ['res','com','ind']:
+    _state_social_df = state_social_df.copy()
+    _state_social_df['sector_abbr'] = s
+    social_dfs.append(state_social_df)
+finance_df = pd.concat(social_dfs, axis='rows')
+finance_df = finance_df.reset_index(drop=True)
+
+social_max = finance_df['social_indicator'].max()
+social_min = finance_df['social_indicator'].min()
+
+#TODO: I'm guessing this will break when MAX_DP and MIN_DP are the same because of division by zero
+# --- inverse normalization of discount rate (i.e. lower social indicator has higher discount rate) ---
+finance_df.loc[finance_df['sector_abbr'] == 'res', 'discount_rate'] = ((config.RES_MAX_DR - config.RES_MIN_DR)/(social_max - social_min)) * (social_max - finance_df['social_indicator']) + config.RES_MIN_DR
+finance_df.loc[finance_df['sector_abbr'] == 'com', 'discount_rate'] = ((config.COM_MAX_DR - config.COM_MIN_DR)/(social_max - social_min)) * (social_max - finance_df['social_indicator']) + config.COM_MIN_DR
+finance_df.loc[finance_df['sector_abbr'] == 'ind', 'discount_rate'] = ((config.IND_MAX_DR - config.IND_MIN_DR)/(social_max - social_min)) * (social_max - finance_df['social_indicator']) + config.IND_MIN_DR
+
+# --- inverse normalization of loan rate(i.e. lower social indicator has higher loan rate ---
+finance_df.loc[finance_df['sector_abbr'] == 'res', 'loan_rate'] = ((config.RES_MAX_LR - config.RES_MIN_LR)/(social_max - social_min)) * (finance_df['social_indicator'] - social_max) + config.RES_MIN_LR
+finance_df.loc[finance_df['sector_abbr'] == 'com', 'loan_rate'] = ((config.COM_MAX_LR - config.COM_MIN_LR)/(social_max - social_min)) * (finance_df['social_indicator'] - social_max) + config.COM_MIN_LR
+finance_df.loc[finance_df['sector_abbr'] == 'ind', 'loan_rate'] = ((config.IND_MAX_LR - config.IND_MIN_LR)/(social_max - social_min)) * (finance_df['social_indicator'] - social_max) + config.IND_MIN_LR
+
+# --- normalization of down payment (i.e. lower social indicator has lower down payment ---
+finance_df.loc[finance_df['sector_abbr'] == 'res', 'down_payment'] = ((config.RES_MAX_DP - config.RES_MIN_DP)/(social_max - social_min)) * (finance_df['social_indicator'] - social_max) + config.RES_MIN_DP
+finance_df.loc[finance_df['sector_abbr'] == 'com', 'down_payment'] = ((config.COM_MAX_DP - config.COM_MIN_DP)/(social_max - social_min)) * (finance_df['social_indicator'] - social_max) + config.COM_MIN_DP
+finance_df.loc[finance_df['sector_abbr'] == 'ind', 'down_payment'] = ((config.IND_MAX_DP - config.IND_MIN_DP)/(social_max - social_min)) * (finance_df['social_indicator'] - social_max) + config.IND_MIN_DP
+
+# --- Write to csv ---
+finance_df.to_csv(os.path.join('input_scenarios','india_base','financing_rates.csv'), index=False)
+
+#%%
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~~~ Load Growth ~~~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
 """
---- load_growth_projection.csv ---
-
 Columns
 -------
     scenario (str) : matches the string from the input sheet
@@ -122,19 +152,18 @@ Columns
     load_multiplier (float) : load growth relative to 2014
 """
 
-"""
---- max_market_share_settings.csv ---
+# --- Create Dataframe from permutations, as values are all similar ---
+load_growth_df = pd.DataFrame().from_records(itertools.product(['Low','Planning','High'],range(2015,2051), ['res','com','ind'], list(set(agent_df['control_reg_id']))))
+load_growth_df.columns = ['scenario','year','sector_abbr','control_reg_id']
 
-Columns
--------
-    metric (str) : i.e. 'payback period'
-    metric_value (float) : i.e 'payback period'
-    max_market_share (float) : survey results estimating maximum market share based on metric_value
-    source (str) : source of survey info
-    business model (str) : how the solar is financed
-    sector_abbr (str) : the sector of the agent
-"""
+load_growth_df.loc[load_growth_df['scenario'] == 'Low', 'load_multiplier'] = config.LOW_LOAD_GROWTH
+load_growth_df.loc[load_growth_df['scenario'] == 'Planning', 'load_multiplier'] = config.PLANNING_LOAD_GROWTH
+load_growth_df.loc[load_growth_df['scenario'] == 'High', 'load_multiplier'] = config.HIGH_LOAD_GROWTH
+#TODO see if this has to be cumulatively multiplied? 
 
+load_growth.to_csv(os.path.join('input_scenarios','india_base','financing_rates.csv'), index=False)
+
+#%%
 """
 --- nem_settings.csv ---
 
@@ -146,24 +175,19 @@ Columns
     nem_system_size_limit_kw (int) : size limit for individual agent system size (kW)
     year_end_excess_sell_rate_usd_per_kwh (float) : payment for excess genration at end of year TODO how is this used? 
 """
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~~ NEM Settings ~~~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
-"""
---- normalized_load.csv OR .json ---
+# --- Create Dataframe from permutations, as values are all similar ---
+nem_df = pd.DataFrame().from_records(itertools.product(['res','com','ind'], list(set(agent_df['control_reg_id'])), range(2015,2051)))
+nem_df.columns = ['sector_abbr','control_reg_id','year']
+nem_df.loc[nem_df['sector_abbr']=='res', 'nem_system_size_limit_kw'] = config.RES_NEM_KW_LIMIT
+nem_df.loc[nem_df['sector_abbr']=='com', 'nem_system_size_limit_kw'] = config.COM_NEM_KW_LIMIT
+nem_df.loc[nem_df['sector_abbr']=='ind', 'nem_system_size_limit_kw'] = config.IND_NEM_KW_LIMIT
+nem_df.to_csv(os.path.join('input_scenarios','base','nem_settings.csv'), index = False)
 
-Columns
--------
-    control_reg_id (int) : integer representation of control_reg
-    kwh (set/list) : 8760 of load normalized between TODO what is this normalized between?
-"""
-
-"""
---- normalized_load.csv OR .json ---
-
-Columns
--------
-    control_reg_id (int) : integer representation of control_reg
-    kwh (set/list) : 8760 of load normalized between TODO what is this normalized between?
-"""
+#%%
 
 """
 --- pv_bass.csv ---
@@ -192,9 +216,11 @@ Columns
     pv_systems_count (int) : existing number of PV systems
 """
 
+#%%
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+"""~~~~~~~~~~~ Rate Escalations ~~~~~~~~~~~~"""
+"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 """
---- rate_escalations.csv ---
-
 Columns
 -------
     source (str) : rate growth planning scenario, from input sheet
@@ -204,6 +230,24 @@ Columns
     escalation_factor (float) : multiplier of rate escalation relative to 2014
 """
 
+# --- Create Dataframe from permutations, as values are all similar ---
+rate_esc_df = pd.DataFrame().from_records(itertools.product(['Planning','Low','High'], range(2015, 2051), ['res','com','ind'], list(set(agent_df['control_reg_id']))))
+rate_esc_df.columns = ['source','year','sector_abbr','control_reg_id']
+
+def escalation_factor_applier(row):
+    multiplier = row['year'] - 2015
+    if row['source'] == 'Planning':
+        esc = 1 + (multiplier * config.PLANNING_RATE_ESCALTION)
+    if row['source'] == 'Low':
+        esc = 1 + (multiplier * config.LOW_RATE_ESCALTION)
+    if row['source'] == 'High':
+        esc = 1 + (multiplier * config.HIGH_RATE_ESCALATION)
+    return esc
+
+rate_esc_df['escalation_factor'] = rate_esc_df.apply(escalation_factor_applier, axis = 1)
+rate_esc_df.to_csv('input_scenarios/base/rate_escalations.csv', index = False)
+
+#%%
 """
 --- solar_resource_hourly.csv OR .json ---
 
@@ -221,4 +265,13 @@ Columns
 -------
     rate_id_alias (int) : integer representation of rate_id
     rate_json (json) : json representation of rate
+"""
+
+"""
+--- normalized_load.csv OR .json ---
+
+Columns
+-------
+    control_reg_id (int) : integer representation of control_reg
+    kwh (set/list) : 8760 of load normalized between TODO what is this normalized between?
 """
